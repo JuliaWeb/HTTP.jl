074bb7a3e99d0d8828da27de96a2fc6d8f896b2e
Daniel Mendel <daniel@danielespeset.com>
2013-04-23 15:39:24 -0400

    Adds encodeURI, decodeURI and parsequerystring

--- src/Httplib.jl
+++ src/Httplib.jl
@@ -18,7 +18,10 @@ export STATUS_CODES,
        Headers,
        Request,
        Response,
-       escapeHTML
+       escapeHTML,
+       encodeURI,
+       decodeURI,
+       parsequerystring
 
 const STATUS_CODES = {
     100 => "Continue",
@@ -119,17 +122,6 @@ RFC1123_datetime(t::CalendarTime) = begin
 end
 RFC1123_datetime() = RFC1123_datetime(now())
 
-# Escape HTML characters
-# 
-# Safety first!
-#
-function escapeHTML(i::String)
-    o = replace(i, r"&(?!(\w+|\#\d+);)", "&amp;")
-    o = replace(o, "<", "&lt;")
-    o = replace(o, ">", "&gt;")
-    replace(o, "\"", "&quot;")
-end
-
 # HTTP Headers
 #
 # Dict Type for HTTP headers
@@ -190,5 +182,75 @@ Response(s::Int, m::String)                        = Response(s, m, headers(), "
 Response(d::String)                                = Response(200, STATUS_CODES[200], d)
 Response(s::Int)                                   = Response(s, STATUS_CODES[s])
 Response()                                         = Response(200)
+
+# Escape HTML characters
+# 
+# Safety first!
+#
+function escapeHTML(i::String)
+    o = replace(i, r"&(?!(\w+|\#\d+);)", "&amp;")
+    o = replace(o, "<", "&lt;")
+    o = replace(o, ">", "&gt;")
+    replace(o, "\"", "&quot;")
+end
+
+# All characters that remain unencoded in URI encoding
+#                                   ( AKA URL encoding 
+#                                     AKA percent-encoding )
+#
+const URIwhitelist = ['A','B','C','D','E','F','G','H','I',
+                      'J','K','L','M','N','O','P','Q','R',
+                      'S','T','U','V','W','X','Y','Z',
+                      'a','b','c','d','e','f','g','h','i',
+                      'j','k','l','m','n','o','p','q','r',
+                      's','t','u','v','w','x','y','z',
+                      '0','1','2','3','4','5','6','7','8',
+                      '9','-','_','.','~']
+
+# decodeURI
+#
+# Decode URI encoded strings
+#
+function decodeURI(encoded::String)
+    enc = split(replace(encoded,"+"," "),"%")
+    decoded = enc[1]
+    for c in enc[2:end]
+        decoded = string(decoded, char(parseint(c[1:2],16)), c[3:end])
+    end
+    decoded
+end
+
+# encodeURI
+#
+# Convert strings to URI encoding
+#
+function encodeURI(decoded::String)
+    encoded = ""
+    for c in decoded
+        encoded = encoded * string(contains(URIwhitelist,c) ? c : "%" * uppercase(hex(int(c))))
+    end
+    encoded
+end
+
+# parsequerystring 
+#
+# Convert a valid querystring to a Dict:
+#
+#    q = "foo=bar&baz=%3Ca%20href%3D%27http%3A%2F%2Fwww.hackershool.com%27%3Ehello%20world%21%3C%2Fa%3E"
+#    parsequerystring(q)
+#    # => ["foo"=>"bar","baz"=>"<a href='http://www.hackershool.com'>hello world!</a>"]
+#
+function parsequerystring(query::String)
+    q = Dict{String,String}()
+    if !contains(query,'=') 
+        return throw("Not a valid query string: $query, must contain at least one key=value pair.")
+    end
+    for set in split(query, "&")
+        key, val = split(set, "=")
+        q[decodeURI(key)] = decodeURI(val)
+    end
+    q
+end
+
 
 end # module Httplib
