bed0c1eb582f3016f556db34c9045b84b36b01c9
Zachary Allaun <zach.allaun@gmail.com>
2013-08-25 17:34:13 -0400

    Revert "When a handler returns a non-Response, merge data into previous Response"
    This reverts commit f06e80f529985bad4854d8464303ad3aa4a4ad8c.

--- src/HttpServer.jl
+++ src/HttpServer.jl
@@ -240,20 +240,6 @@ function process_client(server::Server, client::Client, websockets_enabled::Bool
     event("close", server, client)
 end
 
-function updateresponse(res, status::Int)
-    res.status = status
-    res
-end
-function updateresponse(res, data::String)
-    res.data = data
-    res
-end
-function updateresponse(res, data::(Int, String))
-    res.status = data[1]
-    res.data = data[2]
-    res
-end
-
 # Callback factory for providing `on_message_complete` for `client.parser`
 function message_handler(server::Server, client::Client, websockets_enabled::Bool)
 
@@ -274,12 +260,9 @@ function message_handler(server::Server, client::Client, websockets_enabled::Boo
         local response
 
         try
-            response = Response()
-            out = handle(server.http, req, response)
-            if isa(out, Response)
-                response = out
-            else
-                response = updateresponse(response, out)
+            response = handle(server.http, req, Response()) # Run the server handler
+            if !isa(response, Response)                     # Promote return to Response
+                response = Response(response)
             end
         catch err
             response = Response(500)
