ccc40040f4c7ffd1ec8c45555b26f8936e34948e
tan <tanmaykm@gmail.com>
2013-08-26 23:18:05 +0530

    merging URLParse.jl. fixes #1

--- test/test.jl
+++ test/test.jl
@@ -1,12 +1,12 @@
 using URIParser
 using Base.Test
 
-urls = ["hdfs://user:password@hdfshost:9000/root/folder/file.csv",
+urls = ["hdfs://user:password@hdfshost:9000/root/folder/file.csv#frag",
     "https://user:password@httphost:9000/path1/path2;paramstring?q=a&p=r#frag",
     "https://user:password@httphost:9000/path1/path2?q=a&p=r#frag",
     "https://user:password@httphost:9000/path1/path2;paramstring#frag",
     "https://user:password@httphost:9000/path1/path2#frag",
-    "file:///path/to/file/with?should=work#fine",
+    "file:///path/to/file/with%3fshould%3dwork%23fine",
     "ftp://ftp.is.co.za/rfc/rfc1808.txt", "http://www.ietf.org/rfc/rfc2396.txt", 
     "ldap://[2001:db8::7]/c=GB?objectClass?one", "mailto:John.Doe@example.com", 
     "news:comp.infosystems.www.servers.unix", "tel:+1-816-555-1212", "telnet://192.0.2.16:80/", 
@@ -14,7 +14,8 @@ urls = ["hdfs://user:password@hdfshost:9000/root/folder/file.csv",
 
 failed = 0
 for url in urls
-	if !(string(URI(url)) == url)
+    u = URI(url)
+	if !(string(u) == url) || !isvalid(u)
 		failed += 1
 		println("Test failed for ",url)
 	end
@@ -23,4 +24,19 @@ end
 @test URI("hdfs://user:password@hdfshost:9000/root/folder/file.csv") == URI("hdfs","hdfshost",9000,"/root/folder/file.csv","","","user:password")
 @test URI("google.com","/some/path") == URI("http://google.com:80/some/path")
 
+@test escape("abcdef 1234-=~!@#\$()_+{}|[]a;") == "abcdef%201234-%3d~%21%40%23%24()_%2b%7b%7d%7c%5b%5da%3b"
+@test unescape(escape("abcdef 1234-=~!@#\$()_+{}|[]a;")) == "abcdef 1234-=~!@#\$()_+{}|[]a;"
+
+@test escape_form("abcdef 1234-=~!@#\$()_+{}|[]a;") == "abcdef+1234-%3d~%21%40%23%24()_%2b%7b%7d%7c%5b%5da%3b"
+@test unescape_form(escape_form("abcdef 1234-=~!@#\$()_+{}|[]a;")) == "abcdef 1234-=~!@#\$()_+{}|[]a;"
+
+@test ("user", "password") == userinfo(URI("https://user:password@httphost:9000/path1/path2;paramstring?q=a&p=r#frag"))
+@test URI("https://user:password@httphost:9000/path1/path2;paramstring?q=a&p=r") == defrag(URI("https://user:password@httphost:9000/path1/path2;paramstring?q=a&p=r#frag"))
+
+@test ["dc","example","dc","com"] == path_params(URI("ldap://ldap.example.com/dc=example,dc=com"))[1]
+@test ["servlet","jsessionid","OI24B9ASD7BSSD"] == path_params(URI("http://www.mysite.com/servlet;jsessionid=OI24B9ASD7BSSD"))[1]
+
+@test false == isvalid(URI("file:///path/to/file/with?should=work#fine"))
+@test true == isvalid(URI("file:///path/to/file/with%3fshould%3dwork%23fine"))
 exit(failed)
+
