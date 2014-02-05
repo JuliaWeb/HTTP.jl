7f22213bdf0b1e2a41150e88c64517199c3028f1
Westley <WestleyArgentum@gmail.com>
2014-02-04 23:11:17 -0800

    tests for query + data / minor reformatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,51 +15,82 @@ using Base.Test
 
 # check query params -------
 
-data = JSON.parse(get("http://httpbin.org/get"; query = {"key1" => "value1"}).data)
+data = JSON.parse(get("http://httpbin.org/get"; query = { "key1" => "value1" }).data)
 @test data["args"]["key1"] == "value1"
 
-data = JSON.parse(post("http://httpbin.org/post"; query = {"key1" => "value1",
-                                                           "key2" => "value2" }).data)
+data = JSON.parse(post("http://httpbin.org/post"; query = { "key1" => "value1",
+                                                            "key2" => "value2" }).data)
 @test data["args"]["key1"] == "value1"
 @test data["args"]["key2"] == "value2"
 
-data = JSON.parse(put("http://httpbin.org/put"; query = {"key1" => "value1",
-                                                         "key2" => "value2",
-                                                         "key3" => 3 }).data)
+data = JSON.parse(put("http://httpbin.org/put"; query = { "key1" => "value1",
+                                                          "key2" => "value2",
+                                                          "key3" => 3 }).data)
 @test data["args"]["key1"] == "value1"
 @test data["args"]["key2"] == "value2"
 @test data["args"]["key3"] == "3"
 
-data = JSON.parse(delete("http://httpbin.org/delete"; query = {"key1" => "value1",
-                                                               "key4" => 4.01 }).data)
+data = JSON.parse(delete("http://httpbin.org/delete"; query = { "key1" => "value1",
+                                                                "key4" => 4.01 }).data)
 @test data["args"]["key1"] == "value1"
 @test data["args"]["key4"] == "4.01"
 
-data = JSON.parse(options("http://httpbin.org/get"; query = {"key1" => "value1",
-                                                             "key2" => "value2",
-                                                             "key3" => 3,
-                                                             "key4" => 4.01 }).data)
+data = JSON.parse(options("http://httpbin.org/get"; query = { "key1" => "value1",
+                                                              "key2" => "value2",
+                                                              "key3" => 3,
+                                                              "key4" => 4.01 }).data)
 @test data == nothing
 
 
-#check data -------
+# check data -------
 
-data = JSON.parse(post("http://httpbin.org/post"; data = {"key1" => "value1",
+data = JSON.parse(post("http://httpbin.org/post"; data = { "key1" => "value1",
                                                            "key2" => "value2" }).data)
 @test data["json"]["key1"] == "value1"
 @test data["json"]["key2"] == "value2"
 
-data = JSON.parse(put("http://httpbin.org/put"; data = {"key1" => "value1",
+data = JSON.parse(put("http://httpbin.org/put"; data = { "key1" => "value1",
                                                          "key2" => "value2",
                                                          "key3" => 3 }).data)
 @test data["json"]["key1"] == "value1"
 @test data["json"]["key2"] == "value2"
 @test data["json"]["key3"] == 3
 
-data = JSON.parse(delete("http://httpbin.org/delete"; data = {"key1" => "value1",
+data = JSON.parse(delete("http://httpbin.org/delete"; data = { "key1" => "value1",
                                                                "key4" => 4.01 }).data)
 @test data["json"]["key1"] == "value1"
 @test data["json"]["key4"] == 4.01
 
 
+# query + data -------
 
+data = JSON.parse(post("http://httpbin.org/post"; query = { "qkey1" => "value1",
+                                                            "qkey2" => "value2" },
+                                                  data = { "dkey1" => "data1",
+                                                           "dkey2" => "data2" }).data)
+@test data["args"]["qkey1"] == "value1"
+@test data["args"]["qkey2"] == "value2"
+@test data["json"]["dkey1"] == "data1"
+@test data["json"]["dkey2"] == "data2"
+
+data = JSON.parse(put("http://httpbin.org/put"; query = { "qkey1" => "value1",
+                                                          "qkey2" => "value2",
+                                                          "qkey3" => 3 },
+                                                data = { "dkey1" => "data1",
+                                                         "dkey2" => "data2",
+                                                         "dkey3" => 5 }).data)
+@test data["args"]["qkey1"] == "value1"
+@test data["args"]["qkey2"] == "value2"
+@test data["args"]["qkey3"] == "3"
+@test data["json"]["dkey1"] == "data1"
+@test data["json"]["dkey2"] == "data2"
+@test data["json"]["dkey3"] == 5
+
+data = JSON.parse(delete("http://httpbin.org/delete"; query = { "qkey1" => "value1",
+                                                                "qkey4" => 4.01 },
+                                                      data = { "dkey1" => "data1",
+                                                               "dkey2" => 9.01 }).data)
+@test data["args"]["qkey1"] == "value1"
+@test data["args"]["qkey4"] == "4.01"
+@test data["json"]["dkey1"] == "data1"
+@test data["json"]["dkey2"] == 9.01
