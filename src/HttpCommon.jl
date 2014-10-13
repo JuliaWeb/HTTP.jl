ac23bb4d8ba9474d2ac8327dde6884c32f89d39d
tan <tanmaykm@gmail.com>
2014-10-13 17:04:32 +0530

    fixed deprecation warnings

--- src/HttpCommon.jl
+++ src/HttpCommon.jl
@@ -33,64 +33,64 @@ include("mimetypes.jl")
 
 import Base.show
 
-const STATUS_CODES = {
-    100 => "Continue",
-    101 => "Switching Protocols",
-    102 => "Processing",                          # RFC 2518, obsoleted by RFC 4918
-    200 => "OK",
-    201 => "Created",
-    202 => "Accepted",
-    203 => "Non-Authoritative Information",
-    204 => "No Content",
-    205 => "Reset Content",
-    206 => "Partial Content",
-    207 => "Multi-Status",                        # RFC 4918
-    300 => "Multiple Choices",
-    301 => "Moved Permanently",
-    302 => "Moved Temporarily",
-    303 => "See Other",
-    304 => "Not Modified",
-    305 => "Use Proxy",
-    307 => "Temporary Redirect",
-    400 => "Bad Request",
-    401 => "Unauthorized",
-    402 => "Payment Required",
-    403 => "Forbidden",
-    404 => "Not Found",
-    405 => "Method Not Allowed",
-    406 => "Not Acceptable",
-    407 => "Proxy Authentication Required",
-    408 => "Request Time-out",
-    409 => "Conflict",
-    410 => "Gone",
-    411 => "Length Required",
-    412 => "Precondition Failed",
-    413 => "Request Entity Too Large",
-    414 => "Request-URI Too Large",
-    415 => "Unsupported Media Type",
-    416 => "Requested Range Not Satisfiable",
-    417 => "Expectation Failed",
-    418 => "I'm a teapot",                        # RFC 2324
-    422 => "Unprocessable Entity",                # RFC 4918
-    423 => "Locked",                              # RFC 4918
-    424 => "Failed Dependency",                   # RFC 4918
-    425 => "Unordered Collection",                # RFC 4918
-    426 => "Upgrade Required",                    # RFC 2817
-    428 => "Precondition Required",               # RFC 6585
-    429 => "Too Many Requests",                   # RFC 6585
-    431 => "Request Header Fields Too Large",     # RFC 6585
-    500 => "Internal Server Error",
-    501 => "Not Implemented",
-    502 => "Bad Gateway",
-    503 => "Service Unavailable",
-    504 => "Gateway Time-out",
-    505 => "HTTP Version Not Supported",
-    506 => "Variant Also Negotiates",             # RFC 2295
-    507 => "Insufficient Storage",                # RFC 4918
-    509 => "Bandwidth Limit Exceeded",
-    510 => "Not Extended",                        # RFC 2774
-    511 => "Network Authentication Required"      # RFC 6585
-}
+const STATUS_CODES = Dict([
+    (100, "Continue"),
+    (101, "Switching Protocols"),
+    (102, "Processing"),                          # RFC 2518, obsoleted by RFC 4918
+    (200, "OK"),
+    (201, "Created"),
+    (202, "Accepted"),
+    (203, "Non-Authoritative Information"),
+    (204, "No Content"),
+    (205, "Reset Content"),
+    (206, "Partial Content"),
+    (207, "Multi-Status"),                        # RFC 4918
+    (300, "Multiple Choices"),
+    (301, "Moved Permanently"),
+    (302, "Moved Temporarily"),
+    (303, "See Other"),
+    (304, "Not Modified"),
+    (305, "Use Proxy"),
+    (307, "Temporary Redirect"),
+    (400, "Bad Request"),
+    (401, "Unauthorized"),
+    (402, "Payment Required"),
+    (403, "Forbidden"),
+    (404, "Not Found"),
+    (405, "Method Not Allowed"),
+    (406, "Not Acceptable"),
+    (407, "Proxy Authentication Required"),
+    (408, "Request Time-out"),
+    (409, "Conflict"),
+    (410, "Gone"),
+    (411, "Length Required"),
+    (412, "Precondition Failed"),
+    (413, "Request Entity Too Large"),
+    (414, "Request-URI Too Large"),
+    (415, "Unsupported Media Type"),
+    (416, "Requested Range Not Satisfiable"),
+    (417, "Expectation Failed"),
+    (418, "I'm a teapot"),                        # RFC 2324
+    (422, "Unprocessable Entity"),                # RFC 4918
+    (423, "Locked"),                              # RFC 4918
+    (424, "Failed Dependency"),                   # RFC 4918
+    (425, "Unordered Collection"),                # RFC 4918
+    (426, "Upgrade Required"),                    # RFC 2817
+    (428, "Precondition Required"),               # RFC 6585
+    (429, "Too Many Requests"),                   # RFC 6585
+    (431, "Request Header Fields Too Large"),     # RFC 6585
+    (500, "Internal Server Error"),
+    (501, "Not Implemented"),
+    (502, "Bad Gateway"),
+    (503, "Service Unavailable"),
+    (504, "Gateway Time-out"),
+    (505, "HTTP Version Not Supported"),
+    (506, "Variant Also Negotiates"),             # RFC 2295
+    (507, "Insufficient Storage"),                # RFC 4918
+    (509, "Bandwidth Limit Exceeded"),
+    (510, "Not Extended"),                        # RFC 2774
+    (511, "Network Authentication Required")      # RFC 6585
+])
 
 # HTTP method bitmasks and indexes, allow for fancy GET | POST | UPDATE style APIs.
 typealias HttpMethodBitmask Int
@@ -105,15 +105,15 @@ const HttpMethodBitmasks = HttpMethodBitmask[
     (const HEAD    = 2^6)
 ]
 
-const HttpMethodNameToBitmask = (String => HttpMethodBitmask)[
-    "GET"     => GET,
-    "POST"    => POST,
-    "PUT"     => PUT,
-    "UPDATE"  => UPDATE,
-    "DELETE"  => DELETE,
-    "OPTIONS" => OPTIONS,
-    "HEAD"    => HEAD
-]
+const HttpMethodNameToBitmask = Dict{String, HttpMethodBitmask}([
+    ("GET"     , GET),
+    ("POST"    , POST),
+    ("PUT"     , PUT),
+    ("UPDATE"  , UPDATE),
+    ("DELETE"  , DELETE),
+    ("OPTIONS" , OPTIONS),
+    ("HEAD"    , HEAD)
+])
 
 const HttpMethodBitmaskToName = (HttpMethodBitmask => String)[v => k for (k, v) in HttpMethodNameToBitmask]
 
@@ -133,10 +133,12 @@ RFC1123_datetime() = RFC1123_datetime(Dates.now(Dates.UTC))
 # `headers()` for building default Response Headers
 #
 typealias Headers Dict{String,String}
-headers() = (String => String)[ "Server" => "Julia/$VERSION",
-                                "Content-Type" => "text/html; charset=utf-8",
-                                "Content-Language" => "en",
-                                "Date" => RFC1123_datetime()]
+headers() = Dict{String,String}([ 
+    ("Server"            , "Julia/$VERSION"),
+    ("Content-Type"      , "text/html; charset=utf-8"),
+    ("Content-Language"  , "en"),
+    ("Date"              , RFC1123_datetime())
+])
 
 # HTTP request
 #
@@ -152,7 +154,7 @@ type Request
     headers::Headers
     data::String
 end
-Request() = Request("", "", (String=>String)[], "")
+Request() = Request("", "", Dict{String,String}(), "")
 
 # HTTP response
 #
@@ -193,7 +195,7 @@ function FileResponse(filename)
         s = open(readbytes,filename)
         (_, ext) = splitext(filename)
         mime = length(ext)>1 && haskey(mimetypes,ext[2:end]) ? mimetypes[ext[2:end]] : "application/octet-stream"
-        Response(200, Dict{String,String}({"Content-Type"},{mime}), s)
+        Response(200, Dict{String,String}([("Content-Type",mime)]), s)
     else
         Response(404, "Not Found - file $filename could not be found")
     end         
