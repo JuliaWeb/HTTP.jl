c7d0090a2fcf8a4db5a5e5b09bb04c584ec4ede9
Iain Dunning <iaindunning@gmail.com>
2015-09-05 19:14:13 -0400

    Begone FactCheck

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,18 @@
 using HttpCommon
-using FactCheck
+using Base.Test
 
-facts("HttpCommon utility functions") do
+# Escape HTML
+@test escapeHTML("<script type='text/javascript'>alert('sucker');</script> foo bar") ==
+        "&lt;script type='text/javascript'&gt;alert('sucker');&lt;/script&gt; foo bar"
 
-    context("Escape HTML") do
-        @fact escapeHTML("<script type='text/javascript'>alert('sucker');</script> foo bar") -->
-            "&lt;script type='text/javascript'&gt;alert('sucker');&lt;/script&gt; foo bar"
-    end
+# Decode URI
+@test decodeURI("%3Ca+href%3D%27foo%27%3Ebar%3C%2Fa%3Erun%26%2B%2B") ==
+        "<a href='foo'>bar</a>run&++"
 
-    context("Decode URI") do
-        @fact decodeURI("%3Ca+href%3D%27foo%27%3Ebar%3C%2Fa%3Erun%26%2B%2B") -->
-            "<a href='foo'>bar</a>run&++"
-    end
+# Encode URI
+@test encodeURI("<a href='foo'>bar</a>run&++") ==
+        "%3Ca%20href%3D%27foo%27%3Ebar%3C%2Fa%3Erun%26%2B%2B"
 
-    context("Encode URI") do
-        @fact encodeURI("<a href='foo'>bar</a>run&++") -->
-            "%3Ca%20href%3D%27foo%27%3Ebar%3C%2Fa%3Erun%26%2B%2B"
-    end
-
-    context("Parse URL query strings") do
-        @fact parsequerystring("foo=%3Ca%20href%3D%27foo%27%3Ebar%3C%2Fa%3Erun%26%2B%2B&bar=123") -->
-            Dict("foo" => "<a href='foo'>bar</a>run&++", "bar" => "123")
-    end
-
-end
-
-# Throw error if any tests fails
-FactCheck.exitstatus()
+# Parse URL query strings
+@test parsequerystring("foo=%3Ca%20href%3D%27foo%27%3Ebar%3C%2Fa%3Erun%26%2B%2B&bar=123") ==
+        Dict("foo" => "<a href='foo'>bar</a>run&++", "bar" => "123")
