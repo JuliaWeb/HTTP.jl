b31284bd8a4891037ad718d59595fd7934f501f4
Iain Dunning <iaindunning@gmail.com>
2015-09-09 21:25:06 -0400

    Cover some error paths

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,6 +26,8 @@ end
 
 @test URI("hdfs://user:password@hdfshost:9000/root/folder/file.csv") == URI("hdfs","hdfshost",9000,"/root/folder/file.csv","","","user:password")
 @test URI("google.com","/some/path") == URI("http://google.com:80/some/path")
+g = URI("google.com","/some/path")
+@test URI(g,port=160) == URI("http://google.com:160/some/path")
 
 @test escape("abcdef αβ 1234-=~!@#\$()_+{}|[]a;") == "abcdef%20%CE%B1%CE%B2%201234-%3D~%21%40%23%24()_%2B%7B%7D%7C%5B%5Da%3B"
 @test unescape(escape("abcdef 1234-=~!@#\$()_+{}|[]a;")) == "abcdef 1234-=~!@#\$()_+{}|[]a;"
@@ -40,8 +42,19 @@ end
 @test ["servlet","jsessionid","OI24B9ASD7BSSD"] == path_params(URI("http://www.mysite.com/servlet;jsessionid=OI24B9ASD7BSSD"))[1]
 
 @test Dict("q"=>"a","p"=>"r") == query_params(URI("https://httphost/path1/path2;paramstring?q=a&p=r#frag"))
+@test Dict("q"=>"a","malformed"=>"") == query_params(URI("https://foo.net/?q=a&malformed"))
 
 @test false == isvalid(URI("file:///path/to/file/with?should=work#fine"))
 @test true == isvalid(URI("file:///path/to/file/with%3fshould%3dwork%23fine"))
 
 @test URI("s3://bucket/key") == URI("s3","bucket",0,"/key")
+
+@test sprint(show, URI("http://google.com")) == "URI(http://google.com/)"
+
+# Error paths
+# Non-ASCII characters
+@test_throws ErrorException URI("http://🍕.com")
+# Unexpected start of URL
+@test_throws ErrorException URI(".google.com")
+# Unexpected character after scheme
+@test_throws ErrorException URI("ht!tp://google.com")
