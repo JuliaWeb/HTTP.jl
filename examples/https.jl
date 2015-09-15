a2a3c9dd21383a5458c4e28f887f2ebb360731b6
Tim Lebel <tim.lebel@multiscalehn.com>
2015-09-15 15:11:53 -0700

    Fixed the HTTPS example

--- examples/https.jl
+++ examples/https.jl
@@ -1,37 +1,24 @@
-"""
-Example of using HttpServer with HTTPS protocol:
+# Generate a certificate and key if they do not exist
 
-1) Create a self-signed SSL Certificate
-    - Generate a private key
-        openssl genrsa -des3 -out server.key 1024
+rel(p::String) = joinpath(dirname(@__FILE__), p)
+if !isfile("keys/server.crt")
+  @unix_only begin
+    run(`mkdir -p $(rel("keys"))`)
+    run(`openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout
+      $(rel("keys/server.key")) -out $(rel("keys/server.crt"))`)
+  end
+end
 
-    - Generate a CSR (Certificate Signing Request)
-        openssl req -new -key server.key -out server.csr -subj "/L=github.com/O=JuliaWeb/OU=HttpServer/CN=localhost"
+# Simple HTTPS Server
 
-    - Remove Passphrase from Key
-        cp server.key server.key.org
-        openssl rsa -in server.key.org -out server.key
+using MbedTLS, HttpServer
 
-    - Generating a Self-Signed Certificate
-        openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-
-2) Add certificate/private key pair to a certificate store
-
-3) Run server with the store which contains self-signed SSL certificate
-"""
-
-# Adding certificate/private key pair to certificate store
-using GnuTLS
-cert_store = GnuTLS.CertificateStore()
-GnuTLS.load_certificate(cert_store, "server.crt", "server.key", true)
-
-# Setting up simple server
-using HttpServer
-http = HttpHandler() do req::Request, res::Response
+http = HttpHandler() do req, res
     Response("Hello Secure World!")
 end
-http.events["listen"] = (saddr) -> println("Running on https://$saddr (Press CTRL+C to quit)")
 
-# Running server in HTTPS mode
 server = Server(http)
-run(server, port=8000, ssl=cert_store)
+cert = MbedTLS.crt_parse_file(rel("keys/server.crt"))
+key = MbedTLS.parse_keyfile(rel("keys/server.key"))
+
+run(server, port=8002, ssl=(cert, key))
