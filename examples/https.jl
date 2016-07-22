34fa94fe6fb1cc031a0442fbdac5ae0e71ed9e2a
Jacob Quinn <quinn.jacobd@gmail.com>
2016-07-22 13:28:01 -0600

    Fix missing use of `rel`

--- examples/https.jl
+++ examples/https.jl
@@ -1,7 +1,7 @@
 # Generate a certificate and key if they do not exist
 
 rel(p::String) = joinpath(dirname(@__FILE__), p)
-if !isfile("keys/server.crt")
+if !isfile(rel("keys/server.crt"))
   @unix_only begin
     run(`mkdir -p $(rel("keys"))`)
     run(`openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout
