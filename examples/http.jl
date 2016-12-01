5995aa99e4a79df821494542b033d101a7c52de1
quinnj <quinn.jacobd@gmail.com>
2016-11-30 20:36:05 -0700

    Remove unused files

--- examples/http.jl
+++
@@ -1,9 +0,0 @@
-using HttpServer
-
-http = HttpHandler() do req::Request, res::Response
-    Response("Hello World")
-end
-http.events["listen"] = (saddr) -> println("Running on https://$saddr (Press CTRL+C to quit)")
-
-server = Server(http)
-run(server, host=IPv4(127,0,0,1), port=8000)
