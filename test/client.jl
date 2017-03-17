790671ffa880bc172a12c8ade6ce2df353254d2d
quinnj <quinn.jacobd@gmail.com>
2017-03-17 08:11:47 -0600

    Make test more robust to testing site

--- test/client.jl
+++ test/client.jl
@@ -31,7 +31,9 @@ for sch in ("http", "https")
 
     @test HTTP.status(HTTP.get("$sch://httpbin.org/encoding/utf8")) == 200
 
-    @test HTTP.headers(HTTP.get("$sch://httpbin.org/response-headers"; query=Dict("hey"=>"dude")))["Hey"] == "dude"
+    r = HTTP.get("$sch://httpbin.org/response-headers"; query=Dict("hey"=>"dude"))
+    h = HTTP.headers(r)
+    @test (haskey(h, "Hey") ? h["Hey"] == "dude" : h["hey"] == "dude")
 
     r = HTTP.get("$sch://httpbin.org/cookies")
     body = take!(String, r)
