e5c3c08566924f299259a1f230d4d0bbe2ab3d80
Jacob Quinn <quinn.jacobd@gmail.com>
2018-01-25 09:46:45 -0700

    Re-implement automatic Content-Type detection using the sniff algorithm

---
+++ src/ContentTypeRequest.jl
@@ -0,0 +1,27 @@
+module ContentTypeDetection
+
+import ..Layer, ..request
+using ..URIs
+using ..Pairs: getkv, setkv
+import ..sniff
+import ..Form
+using ..Messages
+import ..MessageRequest: bodylength, bodybytes
+import ..@debug, ..DEBUG_LEVEL
+
+abstract type ContentTypeDetectionLayer{Next <: Layer} <: Layer end
+export ContentTypeDetectionLayer
+
+function request(::Type{ContentTypeDetectionLayer{Next}},
+                 method::String, url::URI, headers, body; kw...) where Next
+
+    if getkv(headers, "Content-Type", "") == "" &&
+        !isa(body, Form) && bodylength(body) != unknown_length && bodylength(body) > 0
+        sn = sniff(bodybytes(body))
+        setkv(headers, "Content-Type", sn)
+        @debug 1 "setting Content-Type header to: $sn"
+    end
+    return request(Next, method, url, headers, body; kw...)
+end
+
+end # module
