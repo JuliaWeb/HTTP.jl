JuliaWeb/HTTP.jl

commit 2cea2f7e093cf3025d4516523a225ba225611021

Fix WebSockets close handshake

src/Streams.jl
@@ -118,14 +118,16 @@ function IOExtras.closewrite(http::Stream{Response})
 end
 
 function IOExtras.closewrite(http::Stream{Request})
-    @require iswritable(http)
 
-    closebody(http)
-    closewrite(http.stream)
+    if iswritable(http)
+        closebody(http)
+        closewrite(http.stream)
+    end
 
     if hasheader(http.message, "Connection", "close") ||
        http.message.version < v"1.1" &&
-      !hasheader(http.message, "Connection", "keep-alive")
+      !hasheader(http.message, "Connection", "keep-alive") ||
+      !hasheader(http.message, "Connection", "upgrade")
 
         @debug 1 "✋  \"Connection: close\": $(http.stream)"
         close(http.stream)

src/WebSockets.jl
@@ -59,7 +59,7 @@ end
 is_websocket_upgrade(r::HTTP.Message) =
     (r isa HTTP.Request && r.method == "GET" || r.status == 101) &&
     HTTP.hasheader(r, "Connection", "upgrade") &&
-    HTTP.hasheader(r, "Upgrade", "webscoket")
+    HTTP.hasheader(r, "Upgrade", "websocket")
 
 
 function check_upgrade(http)
@@ -111,7 +111,9 @@ function open(f::Function, url; binary=false, verbose=false, kw...)
         end
 
         io = ConnectionPool.getrawstream(http)
-        f(WebSocket(io; binary=binary))
+        ws = WebSocket(io; binary=binary)
+        f(ws)
+        close(ws)
     end
 end
 
@@ -143,7 +145,9 @@ function upgrade(f::Function, http::HTTP.Stream; binary=false)
     startwrite(http)
 
     io = ConnectionPool.getrawstream(http)
-    f(WebSocket(io; binary=binary, server=true))
+    ws = WebSocket(io; binary=binary, server=true)
+    f(ws)
+    close(ws)
 end
 
 
@@ -221,7 +225,7 @@ function Base.close(ws::WebSocket)
     if !ws.txclosed
         closewrite(ws)
     end
-    while !ws.rxclosed
+    while !eof(ws) # FIXME Timeout in case other end does not send CLOSE?
         readframe(ws)
     end
     close(ws.io)

test/WebSockets.jl
@@ -25,4 +25,28 @@ for s in ["ws", "wss"]
 
 end
 
+p = UInt16(8000)
+@async HTTP.listen("127.0.0.1",p) do http
+    if HTTP.WebSockets.is_websocket_upgrade(http.message)
+        HTTP.WebSockets.upgrade(http) do ws
+            while !eof(ws)
+                data = readavailable(ws)
+                write(ws,data)
+            end
+        end
+    end
+end
+
+sleep(2)
+
+info("Testing local server...")
+HTTP.WebSockets.open("ws://127.0.0.1:$(p)") do ws
+    write(ws, "Foo")
+    @test String(readavailable(ws)) == "Foo"
+
+    write(ws, "Bar")
+    @test String(readavailable(ws)) == "Bar"
+end
+
+
 end # testset

commit 29ae3fe4190d86b27914e3f28fc81132a9c7ddec

fix broken logic introduced by 2cea2f7e093cf3025d4516523a225ba225611021 in IOExtras.closewrite(http::Stream{Request})

src/Streams.jl
@@ -125,9 +125,9 @@ function IOExtras.closewrite(http::Stream{Request})
     end
 
     if hasheader(http.message, "Connection", "close") ||
+       hasheader(http.message, "Connection", "upgrade") ||
        http.message.version < v"1.1" &&
-      !hasheader(http.message, "Connection", "keep-alive") ||
-      !hasheader(http.message, "Connection", "upgrade")
+      !hasheader(http.message, "Connection", "keep-alive")
 
         @debug 1 "✋  \"Connection: close\": $(http.stream)"
         close(http.stream)