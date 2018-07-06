d4db24251050d653f51321559d9755e181f29956
oxinabox <oxinabox@ucc.asn.au>
2018-07-06 23:19:26 +0800

    =New download

---
+++ src/download.jl
@@ -0,0 +1,114 @@
+using .Pairs
+
+function try_get_filename_from_headers(headers)
+    content_disp = getkv(headers, "Content-Disposition")
+    if content_disp != nothing
+        # extract out of Content-Disposition line
+        # rough version of what is needed in https://github.com/JuliaWeb/HTTP.jl/issues/179
+        filename_part = match(r"filename\s*=\s*(.*)", content_disp)
+        if filename_part != nothing
+            filename = filename_part[1]
+            quoted_filename = match(r"\"(.*)\"", filename)
+            if quoted_filename != nothing
+                # It was in quotes, so it will be double escaped
+                filename = unescape_string(quoted_filename[1])
+            end
+            return filename
+        end
+    end
+    return nothing
+end
+
+function try_get_filename_from_remote_path(target)
+    target == "" && return nothing
+    filename = basename(target)
+    if filename == ""
+        try_get_filename_from_remote_path(dirname(target))
+    else
+        filename
+    end
+end
+
+
+determine_file(::Nothing, resp) = determine_file(tempdir(), resp)
+# ^ We want to the filename if possible because extension is useful for FileIO.jl
+
+function determine_file(path, resp)
+    # get the name
+    name = if isdir(path)
+        # got to to workout what file to put there
+        filename = something(
+                        try_get_filename_from_headers(resp.headers),
+                        try_get_filename_from_remote_path(resp.request.target),
+                        basename(tempname()) # fallback, basically a random string
+                    )
+        joinpath(path, filename)
+    else
+        # It is a file, we are done.
+        path
+    end
+
+    # get the extension, if we are going to save it in encoded form.
+    if header(resp, "Content-Encoding") == "gzip"
+        name *= ".gz"
+    end
+    name
+end
+
+"""
+    download(url, [local_path])
+
+Similar to `Base.download` this downloads a file, returning the filename.
+If the `local_path`:
+ - is not provided, then it is saved in a temporary directory
+ - if part to a directory is provided then it is saved into that directory
+ - otherwise the local path is uses as the filename to save to.
+
+When saving into a directory, the filename is determined (where possible),
+from the rules of the HTTP.
+"""
+function download(url::AbstractString, local_path=nothing)
+    local file
+    HTTP.open("GET", url) do stream
+        resp = startread(stream)
+        file = determine_file(local_path, resp)
+        @show file
+        @show resp
+        total_bytes = parse(Float64, getkv(resp.headers, "Content-Length", "NaN"))
+        downloaded_bytes = 0
+        start_time = now()
+        prev_time = now()
+
+        function report_callback()
+            prev_time = now()
+            taken_time = (prev_time - start_time).value / 1000 # in seconds
+            average_speed = downloaded_bytes / taken_time
+            remaining_bytes = total_bytes - downloaded_bytes
+            remaining_time = remaining_bytes/average_speed
+            completion_progress = downloaded_bytes/total_bytes
+        
+            @info("Downloading",
+                  source=url,
+                  dest = file,
+                  progress = completion_progress,
+                  taken_time,
+                  remaining_time,
+                  average_speed,
+                  downloaded_bytes,
+                  remaining_bytes,
+                  total_bytes,
+            )
+        end
+
+        open(file, "w") do fh
+            while(!eof(stream))
+                downloaded_bytes += write(fh, readavailable(stream))
+                if now() - prev_time > Millisecond(500)
+                    report_callback()
+                end
+            end
+        end
+        report_callback()
+    end
+    file
+end
---
+++ test/download.jl
@@ -0,0 +1,39 @@
+include("compat.jl")
+using HTTP
+
+@testset "HTTP.download" begin
+    @testset "Content-Disposition" begin
+        invalid_content_disposition_fn = HTTP.download(
+            "http://test.greenbytes.de/tech/tc2231/attonlyquoted.asis")
+        @test isfile(invalid_content_disposition_fn)
+        @test basename(invilid_content_disposition_fn) == "attonlyquoted.asis" # just last part  of name
+
+        content_disposition_fn = HTTP.download(
+            "http://test.greenbytes.de/tech/tc2231/attwithasciifilenamepdf.asis")
+        @test isfile(content_disposition_fn)
+        @test basename(content_disposition_fn) == "foo.pdf"
+
+        escaped_content_disposition_fn = HTTP.download(
+            "http://test.greenbytes.de/tech/tc2231/attwithasciifnescapedquote.asis")
+        @test isfile(escaped_content_disposition_fn)
+        @test basename(escaped_content_disposition_fn) == "\"quoting\" tested.html"
+    end
+
+    @testset "Provided Filename" begin
+        provided_filename = tempname()
+        returned_filename = HTTP.download(
+            "http://test.greenbytes.de/tech/tc2231/attwithasciifilenamepdf.asis",
+            provided_filename
+        )
+        @test provided_filename == returned_filename
+        @test isfile(provided_filename)
+
+
+    end
+
+    @testset "Content-Encoding" begin
+        zip_content_encoding_fn = HTTP.download("https://httpbin.org/gzip")
+        @test isfile(gzip_content_encoding_fn)
+        @test last(splitext(gzip_content_encoding_fn)) == ".gz"
+    end
+end
