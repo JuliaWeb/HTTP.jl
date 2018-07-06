a908fb5c869654e64f76534859e0ee5018a648ea
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-06 16:23:01 +0100

    added manual ways of checking for boundary and content

--- src/parsemultipart.jl
+++ src/parsemultipart.jl
@@ -1,3 +1,40 @@
+function find_boundary(bytes::Vector{UInt8}, str, dashes)
+    l = length(bytes)
+    i = 1
+    cons_dash = 0
+    while i+length(str) <= l
+        @inbounds cons_dash = (bytes[i] == 0x2d) ? cons_dash + 1 : 0
+        if cons_dash >= dashes && bytes[(i+1):(i+length(str))] == str
+            return [i-dashes, i+length(str)]
+        end
+        i += 1
+    end
+    nothing
+end
+
+function find_boundary(body::Vector{UInt8}, boundary)
+    m =  match(r"^(-*)(.*)$", boundary)
+    m === nothing && return nothing
+    d, str = m[1], m[2]
+    find_boundary(body, unsafe_wrap(Array{UInt8, 1}, String(str)), length(d))
+end
+
+function find_content(bytes::Vector{UInt8})
+    l = length(bytes)
+    i = 1
+    while i <= l-1
+        matched = false
+        bytes[i] == 0x0d && bytes[i+1] == 0x0d && (matched = true)
+        bytes[i] == 0x0a && bytes[i+1] == 0x0a && (matched = true)
+        matched && return [i-1, i+1]
+        bytes[i] == 0x0d && bytes[i+1] == 0x0a && (i+3 <= l) && bytes[i+2] == 0x0d && bytes[i+3] == 0x0a && (matched = true)
+        matched && return [i-1, i+3]
+
+        i += 1
+    end
+    nothing
+end
+
 function parse_multipart_chunk!(chunk, d)
     re = r"(\n){2,}|(\r){2,}|[\r\n]{4,}"
     Parsers.exec(re, chunk) || return nothing
@@ -23,15 +60,14 @@ function parse_multipart_chunk!(chunk, d)
     push!(d["content"], content)
 end
 
-parse_multipart_body(body::AbstractArray{UInt8}, boundary) =
+parse_multipart_body(body, boundary) =
     parse_multipart_body(String(body), boundary)
 
-function parse_multipart_body(body, boundary)
-    body = String(body)
+function parse_multipart_body(body::AbstractString, boundary)
     dict = Dict(
         "name" => String[],
-        "filename" => Union{String, Void}[],
-        "contenttype" => Union{String, Void}[],
+        "filename" => Union{String, Nothing}[],
+        "contenttype" => Union{String, Nothing}[],
         "content" => Any[]
     )
     re = Regex(boundary)
