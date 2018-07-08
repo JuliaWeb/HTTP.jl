71f121b543ba6b1eb0894a912332d4fa0605f77b
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-08 12:34:48 +0100

    rename

--- src/parsemultipart.jl
+++ src/parsemultipart.jl
@@ -1,6 +1,6 @@
-function find_boundary(bytes::Vector{UInt8}, str, dashes)
+function find_boundary(bytes::AbstractVector{UInt8}, str, dashes; start::Int = 1)
     l = length(bytes)
-    i = 1
+    i = start
     cons_dash = 0
     while i+length(str) <= l
         @inbounds cons_dash = (bytes[i] == 0x2d) ? cons_dash + 1 : 0
@@ -12,35 +12,58 @@ function find_boundary(bytes::Vector{UInt8}, str, dashes)
     nothing
 end
 
-function find_boundary(body::Vector{UInt8}, boundary)
+function find_boundaries(bytes::AbstractVector{UInt8}, str, dashes; start = 1)
+    idxs = Vector{Int}[]
+    j = find_boundary(bytes, str, dashes; start = start)
+    while j !== nothing
+        push!(idxs, j)
+        j = find_boundary(bytes, str, dashes; start = j[2]+1)
+    end
+    return idxs
+end
+
+function find_boundaries(bytes::AbstractVector{UInt8}, boundary; start::Int = 1)
     m =  match(r"^(-*)(.*)$", boundary)
     m === nothing && return nothing
     d, str = m[1], m[2]
-    find_boundary(body, unsafe_wrap(Array{UInt8, 1}, String(str)), length(d))
+    find_boundaries(bytes, unsafe_wrap(Array{UInt8, 1}, String(str)), length(d); start = start)
 end
 
-function find_content(bytes::Vector{UInt8})
+function remove_trailing(bytes::AbstractVector{UInt8}, charlist::AbstractVector{UInt8})
+    i = findlast(in(charlist), bytes)
+    j = i !== nothing ? i-1 : lastindex(bytes)
+    view(bytes, 1:j)
+end
+
+remove_trailing(bytes::AbstractVector{UInt8}, char::UInt8) = remove_trailing(bytes, [char])
+
+function find_returns(bytes::AbstractVector{UInt8})
     l = length(bytes)
     i = 1
-    while i <= l-1
-        matched = false
-        bytes[i] == 0x0d && bytes[i+1] == 0x0d && (matched = true)
-        bytes[i] == 0x0a && bytes[i+1] == 0x0a && (matched = true)
-        matched && return [i-1, i+1]
-        bytes[i] == 0x0d && bytes[i+1] == 0x0a && (i+3 <= l) && bytes[i+2] == 0x0d && bytes[i+3] == 0x0a && (matched = true)
-        matched && return [i-1, i+3]
-
+    returns = UInt8[]
+    returnbytes = [0x0d, 0x0a]
+    while i <= l
+        @inbounds byte = bytes[i]
+        if byte in returnbytes
+            push!(returns, byte)
+            if i==l || !(bytes[i+1] in returnbytes)
+                len = length(returns)
+                uniquelen = length(unique(returns))
+                len >= 2 * uniquelen && return [i-len, i]
+            end
+        else
+            isempty(returns) || empty!(returns)
+        end
         i += 1
     end
     nothing
 end
 
-function parse_multipart_chunk!(chunk, d)
-    re = r"(\n){2,}|(\r){2,}|[\r\n]{4,}"
-    Parsers.exec(re, chunk) || return nothing
-    i, j = re.ovec
-    description = String(chunk[1:i])
-    content = Parsers.nextbytes(re, chunk)
+function parse_multipart_chunk!(d, chunk)
+    i = find_returns(chunk)
+    i == nothing && return
+    description = String(view(chunk, 1:i[1]))
+    content = view(chunk, i[2]+1:lastindex(chunk))
 
     v = match(r"Content-Disposition: form-data; name=\"(.*)\"; filename=\"(.*)\"[\r\n]+Content-Type: (\S*)", description)
     if v !== nothing
@@ -55,28 +78,22 @@ function parse_multipart_chunk!(chunk, d)
         push!(d["contenttype"], nothing)
     end
 
-    re = r"(?:[\r\n]*)(?:-*)$"
-    Parsers.exec(re, content) && (content = content[1:re.ovec[1]])
+    content = remove_trailing(content, 0xd)
+    content = remove_trailing(content, [0x0d, 0x0a])
     push!(d["content"], content)
 end
 
-parse_multipart_body(body, boundary) =
-    parse_multipart_body(String(body), boundary)
-
-function parse_multipart_body(body::AbstractString, boundary)
+function parse_multipart_body(body::Vector{UInt8}, boundary)
     dict = Dict(
         "name" => String[],
         "filename" => Union{String, Nothing}[],
         "contenttype" => Union{String, Nothing}[],
         "content" => Any[]
     )
-    re = Regex(boundary)
-    Parsers.exec(re, body) || return nothing
-    str = Parsers.nextbytes(re, body)
-    while Parsers.exec(re, str)
-        chunk = str[1:re.ovec[1]]
-        isempty(chunk) || parse_multipart_chunk!(chunk, dict)
-        str = Parsers.nextbytes(re, str)
+    idxs = find_boundaries(body, boundary)
+    for i in 1:length(idxs)-1
+        chunk = view(body, idxs[i][2]+1:idxs[i+1][1])
+        parse_multipart_chunk!(dict, chunk)
     end
     return dict
 end
