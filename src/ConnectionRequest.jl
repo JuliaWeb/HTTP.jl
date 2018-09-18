e7641e977ebc0285bf4e0d3ca4cf4d28439e71d0
Sam O'Connor <sam@octech.com.au>
2018-09-19 07:17:11 +1000

    Merge pull request #296 from invenia/ed/errwrap-getconnection
    Wrap UVError/IOError in getconnection with HTTP.IOError

--- src/ConnectionRequest.jl
+++ src/ConnectionRequest.jl
@@ -35,7 +35,12 @@ function request(::Type{ConnectionPoolLayer{Next}}, url::URI, req, body;
     end
 
     IOType = ConnectionPool.Transaction{sockettype(url, socket_type)}
-    io = getconnection(IOType, url.host, url.port; kw...)
+    local io
+    try
+        io = getconnection(IOType, url.host, url.port; kw...)
+    catch e
+        rethrow(isioerror(e) ? IOError(e, "during request($url)") : e)
+    end
 
     try
         if proxy != nothing && target_url.scheme == "https"
