7cde9dbd279234c5d2a6b5fced32883e9c12fd53
Sam O'Connor <sam@octech.com.au>
2018-09-28 00:05:41 +1000

    move Lazy* tests from src/ to test/.

--- src/LazyStrings.jl
+++ src/LazyStrings.jl
@@ -39,8 +39,6 @@ FieldName("  foo: bar", 1) == "foo"
 """
 module LazyStrings
 
-export LazyString, LazyASCII
-
 const WARN_FULL_ITERATION_OF_LAZY_STRING = true
 
 
@@ -146,11 +144,6 @@ Should character `c` at index `i` be ignored?
 isskip(s::LazyASCII, i) = isskip(s, i, getc(s.s, s.i + i - 1))
 isskip(::LazyASCII, i, c) = false
 
-"""
-Translate character `c` to something else.
-"""
-replace(::LazyASCII, i, c) = c, 0
-
 
 """
 Find the index of the first character of the sub-string.
@@ -186,8 +179,6 @@ function scan_string(s::LazyASCII)
         if !isskip(s, i, c)
             n += 1
         end
-        c, r = replace(s, i, c)
-        n -= r
         i, c = next_ic(ss, i)
     end
 
@@ -213,14 +204,17 @@ function _iterate(character, s::LazyASCII, i)
     if isend(s, si, c)
         return nothing
     end
-    while isskip(s, si, c)
-        si, c = next_ic(ss, si)
+    if isskip(s, si, c)
+        while true
+            si, c = next_ic(ss, si)
+            if isend(s, si, c)
+                return nothing
+            end
+            if !isskip(s, si, c)
+                break
+            end
+        end
     end
-    if isend(s, si, c)
-        return nothing
-    end
-    c, n = replace(s, si, c)
-    si += n
     return character(c), si + 2 - s.i
 end
 
@@ -277,140 +271,3 @@ end
 
 
 end # module LazyStrings
-
-#=
-import .LazyStrings.LazyString
-import .LazyStrings.LazyASCII
-
-using Test
-using InteractiveUtils
-
-struct TestLazy{T} <: LazyString
-    s::T
-    i::Int
-end
-
-LazyStrings.isend(::TestLazy, i, c) = c == '\n'
-
-@test TestLazy(" Foo", 2) == "Foo"
-
-@test TestLazy(" Foo\n ", 2) == "Foo"
-
-struct TestLazyASCII{T} <: LazyASCII
-    s::T
-    i::Int
-end
-
-LazyStrings.findstart(s::TestLazyASCII) = findnext(c->c != ' ', s.s, s.i)
-LazyStrings.isend(::TestLazyASCII, i, c) = c == UInt8('\n')
-
-struct TestLazyASCIIB{T} <: LazyASCII
-    s::T
-    i::Int
-end
-
-LazyStrings.findstart(s::TestLazyASCIIB) = findnext(c->c != ' ', s.s, s.i)
-LazyStrings.isend(::TestLazyASCIIB, i, c) = c == UInt8('\n')
-LazyStrings.isskip(::TestLazyASCIIB, i, c) = c == UInt8('_')
-
-struct TestLazyASCIIC{T} <: LazyASCII
-    s::T
-    i::Int
-end
-
-LazyStrings.isend(::TestLazyASCIIC, i, c) = c == UInt8('\n')
-
-function test_lazy_ascii()
-
-    for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
-        s = "Foo"
-        pads = repeat(" ", pada) * s * "\n" * repeat(" ", padb)
-
-        for x in [s, TestLazyASCII(pads, pada + 1)]
-
-            @test x == "Foo"
-            @test x == String(x)
-            @test x == SubString(x)
-
-            @test map(i->isvalid(x, i), 0:4) == [false, true, true, true, false]
-            @test map(i->thisind(x, i), 0:4) == [0, 1, 2, 3, 4]
-            @test map(i->prevind(x, i), 1:4) == [0, 1, 2, 3]
-            @test map(i->nextind(x, i), 0:3) == [1, 2, 3, 4]
-
-            @test map(i->iterate(x, i), 1:4) == [('F', 2),
-                                                 ('o', 3),
-                                                 ('o', 4),
-                                                 nothing]
-
-            @test_throws BoundsError prevind(x, 0)
-            @test_throws BoundsError nextind(x, 4)
-        end
-    end
-
-    for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
-        s = "Fu_m"
-        pads = repeat(" ", pada) * s * "\n" * repeat(" ", padb)
-
-        for x in [TestLazyASCIIB(pads, pada + 1)]
-
-            @test x == "Fum"
-            @test x == String(x)
-            @test x == SubString(x)
-
-            @test map(i->isvalid(x, i), 0:5) == [false, true, true, false, true, false]
-            @test map(i->thisind(x, i), 0:5) == [0, 1, 2, 2, 4, 5]
-            @test map(i->prevind(x, i), 1:5) == [0, 1, 2, 2, 4]
-            @test map(i->nextind(x, i), 0:4) == [1, 2, 4, 4, 5]
-
-            @test map(i->iterate(x, i), 1:5) == [('F', 2),
-                                                 ('u', 3),
-                                                 ('m', 5),
-                                                 ('m', 5),
-                                                 nothing]
-
-            @test_throws BoundsError prevind(x, 0)
-            @test_throws BoundsError nextind(x, 5)
-        end
-    end
-
-    for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
-        s = "Fu_m_"
-        pads = repeat(" ", pada) * s * "\n" * repeat(" ", padb)
-
-        for x in [TestLazyASCIIB(pads, pada + 1)]
-
-            @test x == "Fum"
-            @test x == String(x)
-            @test x == SubString(x)
-
-            @test map(i->isvalid(x, i), 0:6) == [false, true, true, false, true, false, false]
-            @test map(i->thisind(x, i), 0:6) == [0, 1, 2, 2, 4, 4, 6]
-            @test map(i->prevind(x, i), 1:6) == [0, 1, 2, 2, 4, 4]
-            @test map(i->nextind(x, i), 0:5) == [1, 2, 4, 4, 6, 6]
-
-            @test map(i->iterate(x, i), 1:6) == [('F', 2),
-                                                 ('u', 3),
-                                                 ('m', 5),
-                                                 ('m', 5),
-                                                 nothing,
-                                                 nothing]
-
-            @test_throws BoundsError prevind(x, 0)
-            @test_throws BoundsError nextind(x, 6)
-        end
-    end
-
-    @test TestLazyASCIIC("Foo", 1) == "Foo"
-    @test TestLazyASCIIC(" Foo\n ", 1) == " Foo"
-
-    s = TestLazyASCIIC(" Foo\n ", 1)
-
-    str = Base.StringVector(6)
-
-    #@code_native iterate(s)
-    #@code_warntype iterate(s)
-    #@code_native iterate(s, 1)
-    #@code_warntype iterate(s, 1)
-end
-
-=#
---
+++ test/LazyHTTP.jl
@@ -0,0 +1,202 @@
+using HTTP
+
+using HTTP.LazyHTTP
+
+using Test
+
+import .LazyHTTP.RequestHeader
+import .LazyHTTP.ResponseHeader
+
+ifilter(a...) = Base.Iterators.filter(a...)
+
+
+@testset "LazyHTTP" begin
+
+f_eq_s = LazyHTTP.field_isequal_string
+f_eq_f = LazyHTTP.field_isequal_field
+
+@test f_eq_s("A:", 1, "B", 1) == 0
+
+for l in ["Foo-12", "FOO-12", "foo-12"],
+    r =  ["Foo-12", "FOO-12", "foo-12"]
+
+    for (c, i) in [(":", 7), ("\n", 0), ("", 0)]
+        @test f_eq_s("$l$c", 1, "$r", 1) == i
+        @test f_eq_s("$l$c", 1, SubString("$r"), 1) == i
+        @test f_eq_s("$l$c", 1, SubString(" $r ", 2, 7), 1) == i
+        @test f_eq_s("$l$c", 1, " $r", 2) == i
+
+        @test f_eq_f("$l$c", 1, "$r$c", 1) == i
+        @test f_eq_f("$l$c xxx", 1, "$r$c xxx", 1) == i
+        @test f_eq_f("$l$c xxx", 1, "$r$c yyy", 1) == i
+    end
+
+    @test f_eq_s("$l:", 1, "$r:", 1) == 0
+    @test f_eq_s("$l:", 1, " $r:", 2) == 0
+    @test f_eq_s("$l:", 1, " $r ", 2) == 0
+    @test f_eq_s("$l:", 1, SubString("$r", 1, 5), 1) == 0
+
+    @test f_eq_s("$l\n:", 1, "$r\n", 1) == 0
+
+    @test f_eq_s("$l:a", 1, "$r:a", 1) == 0
+
+    @test f_eq_f("$l\n", 1, "$r\n", 1) == 0
+    @test f_eq_f("$l", 1, "$r", 1) == 0
+    @test f_eq_f("$l:", 1, "$r", 1) == 0
+    @test f_eq_f("$l", 1, "$r:", 1) == 0
+    @test f_eq_f("$l: xxx", 1, "$r: yyy", 2) == 0
+    @test f_eq_f("$l: xxx", 2, "$r: yyy", 1) == 0
+end
+
+s = "GET / HTP/1.1\r\n\r\n"
+h = RequestHeader(s)
+
+@show h.method
+@show h.target
+@show h.version
+
+s = "SOMEMETHOD HTTP/1.1\r\nContent-Length: 0\r\n\r\n"
+
+h = RequestHeader(s)
+
+@show h.method
+@show h.target
+@show h.version
+
+s = "HTTP/1.1 200\r\n" *
+    "A: \r\n" *
+    " \r\n" *
+    "B: B\r\n" *
+    "C:\r\n" *
+    " C\r\n" *
+    "D: D \r\n" *
+    " D   \r\n" *
+    " D   \r\n" *
+    "\r\n"
+
+h = ResponseHeader(s)
+
+@test h["A"] == " "
+@test h["B"] == "B"
+@test h["C"] == " C"
+@test h["D"] == "D  D    D"
+
+s = "HTTP/1.1 200 OK\r\n" *
+    "Foo: \t Bar Bar\t  \r\n" *
+    "X: Y  \r\n" *
+    "X:  Z \r\n" *
+    "XX: Y  \r\n" *
+    "XX:  Z \r\n" *
+    "Field: Value\n folded \r\n more fold\n" *
+    "Blah: x\x84x" *
+    "\r\n" *
+    "\r\n"
+
+h = ResponseHeader(s)
+
+@test (@allocated h = ResponseHeader(s)) <= 32
+
+@test h.status == 200
+@test (@allocated h.status) == 0
+@test h.version == v"1.1"
+@test (@allocated h.version) <= 48
+
+@test h["X"] == "Y"
+@test collect(ifilter(p -> p.first == "X", h)) == ["X" => "Y", "X" => "Z"]
+@test h["XX"] == "Y"
+@test collect(ifilter(p -> p.first == "XX", h)) == ["XX" => "Y", "XX" => "Z"]
+
+@test collect(keys(h)) == ["Foo", "X", "X", "XX", "XX", "Field", "Blah"]
+if LazyHTTP.ENABLE_OBS_FOLD
+@test collect(h) == ["Foo" => "Bar Bar",
+                     "X" => "Y",
+                     "X" => "Z",
+                     "XX" => "Y",
+                     "XX" => "Z",
+                     "Field" => "Value folded  more fold",
+                     "Blah" => "x\x84x"]
+else
+@test h["Field"] != "Foo"
+@test h["Field"] != "Valu"
+@test_throws LazyHTTP.ParseError h["Field"] == "Value"
+@test_throws LazyHTTP.ParseError h["Field"] == "Value folded  more fold"
+@test [n => h[n] for n in ifilter(x->x != "Field", keys(h))] ==
+    ["Foo" => "Bar Bar",
+     "X" => "Y",
+     "X" => "Z",
+     "XX" => "Y",
+     "XX" => "Z",
+     "Blah" => "x\x84x"]
+end
+
+@test (@allocated keys(h)) <= 16
+@test iterate(keys(h)) == ("Foo", 18)
+@test (@allocated iterate(keys(h))) <= 80
+
+@test SubString(h["Foo"]).string == s
+@test SubString(h["Blah"]).string == s
+@test SubString(h["X"]).string == s
+if LazyHTTP.ENABLE_OBS_FOLD
+@test SubString(h["Field"]).string != s
+end
+
+@test (@allocated SubString(h["Blah"])) <= 64
+
+@test all(n->SubString(n).string == s, keys(h))
+
+@test haskey(h, "Foo")
+@test haskey(h, "FOO")
+@test haskey(h, "foO")
+@test (@allocated haskey(h, "Foo")) == 0
+@test (@allocated haskey(h, "XXx")) == 0
+
+if LazyHTTP.ENABLE_OBS_FOLD
+@test [h[n] for n in keys(h)] == ["Bar Bar",
+                                  "Y",
+                                  "Z",
+                                  "Y",
+                                  "Z",
+                                  "Value folded  more fold",
+                                  "x\x84x"]
+
+@test [h[n] for n in keys(h)] == [x for x in values(h)]
+@test [h[n] for n in keys(h)] == [String(x) for x in values(h)]
+@test [h[n] for n in keys(h)] == [SubString(x) for x in values(h)]
+else
+@test [h[n] for n in ifilter(x->x != "Field", keys(h))] == ["Bar Bar",
+                                  "Y",
+                                  "Z",
+                                  "Y",
+                                  "Z",
+                                  "x\x84x"]
+end
+
+
+
+s = "GET /foobar HTTP/1.1\r\n" *
+    "Foo: \t Bar Bar\t  \r\n" *
+    "X: Y  \r\n" *
+    "Field: Value\n folded \r\n more fold\n" *
+    "Blah: x\x84x" *
+    "\r\n" *
+    "\r\n"
+
+@test !isvalid(RequestHeader(s))
+@test isvalid(RequestHeader(s); obs=true)
+
+@test LazyHTTP.method(RequestHeader(s)) == "GET"
+@test LazyHTTP.target(RequestHeader(s)) == "/foobar"
+@test LazyHTTP.version(RequestHeader(s)) == v"1.1"
+
+@test RequestHeader(s).method == "GET"
+@test RequestHeader(s).target == "/foobar"
+@test RequestHeader(s).version == v"1.1"
+@test LazyHTTP.version_is_1_1(RequestHeader(s))
+
+
+h = RequestHeader(s)
+@test h.method == "GET"
+@test (@allocated h.method) <= 32
+
+
+end # testset "LazyHTTP"
