74851428edf95589241545a04bfb87c81a16fad8
Sam O'Connor <sam@octech.com.au>
2018-09-28 19:39:42 +1000

    Improve tests for LazyString nextind and isvalid.

--- test/LazyStrings.jl
+++ test/LazyStrings.jl
@@ -30,8 +30,13 @@ struct TestLazyASCIIB{T} <: LazyASCII
 end
 
 LazyStrings.findstart(s::TestLazyASCIIB) = findnext(c->c != ' ', s.s, s.i)
-LazyStrings.isend(::TestLazyASCIIB, i, c) = c == UInt8('\n')
 LazyStrings.isskip(::TestLazyASCIIB, i, c) = c == UInt8('_')
+function LazyStrings.isend(s::TestLazyASCIIB, i, c)
+    while LazyStrings.isskip(s, i, c)
+        i, c = LazyStrings.next_ic(s.s, i)
+    end
+    return c == UInt8('\n')
+end
 
 struct TestLazyASCIIC{T} <: LazyASCII
     s::T
@@ -58,9 +63,9 @@ for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
         @test x == SubString(x)
 
         @test map(i->isvalid(x, i), 0:4) == [false, true, true, true, false]
-        @test map(i->thisind(x, i), 0:4) == [0, 1, 2, 3, 4]
-        @test map(i->prevind(x, i), 1:4) == [0, 1, 2, 3]
-        @test map(i->nextind(x, i), 0:3) == [1, 2, 3, 4]
+        @test map(i->thisind(x, i), 0:3) == [0, 1, 2, 3]
+        @test map(i->prevind(x, i), 1:4) ==    [0, 1, 2, 3]
+        @test map(i->nextind(x, i), 0:3) == [1, 2, 3, ncodeunits(x)+1]
 
         @test map(i->iterate(x, i), 1:4) == [('F', 2),
                                              ('o', 3),
@@ -68,7 +73,7 @@ for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
                                              nothing]
 
         @test_throws BoundsError prevind(x, 0)
-        @test_throws BoundsError nextind(x, 4)
+        @test_throws BoundsError nextind(x, ncodeunits(x)+1)
     end
 end
 
@@ -82,10 +87,15 @@ for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
         @test x == String(x)
         @test x == SubString(x)
 
-        @test map(i->isvalid(x, i), 0:5) == [false, true, true, false, true, false]
-        @test map(i->thisind(x, i), 0:5) == [0, 1, 2, 2, 4, 5]
-        @test map(i->prevind(x, i), 1:5) == [0, 1, 2, 2, 4]
-        @test map(i->nextind(x, i), 0:4) == [1, 2, 4, 4, 5]
+        index_valid(x) = i->(isvalid(x, i) ? 1 : 0)
+                                          #     F  u  _  m
+                                          #  0  1  2  3  4  5
+        @test map(index_valid(x), 0:5)   == [0, 1, 1, 1, 0, 0]
+        @test map(i->thisind(x, i), 0:4) == [0, 1, 2, 3, 3]
+        @test thisind(x, 5) == 3 || thisind(x, 5) == ncodeunits(x) + 1
+        @test map(i->prevind(x, i), 1:5) ==    [0, 1, 2, 3, 3]
+        @test map(i->nextind(x, i), 0:4) == [1, 2, 3, ncodeunits(x) + 1,
+                                                      ncodeunits(x) + 1]
 
         @test map(i->iterate(x, i), 1:5) == [('F', 2),
                                              ('u', 3),
@@ -94,7 +104,7 @@ for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
                                              nothing]
 
         @test_throws BoundsError prevind(x, 0)
-        @test_throws BoundsError nextind(x, 5)
+        @test_throws BoundsError nextind(x, ncodeunits(x) + 1)
     end
 end
 
@@ -108,10 +118,15 @@ for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
         @test x == String(x)
         @test x == SubString(x)
 
-        @test map(i->isvalid(x, i), 0:6) == [false, true, true, false, true, false, false]
-        @test map(i->thisind(x, i), 0:6) == [0, 1, 2, 2, 4, 4, 6]
-        @test map(i->prevind(x, i), 1:6) == [0, 1, 2, 2, 4, 4]
-        @test map(i->nextind(x, i), 0:5) == [1, 2, 4, 4, 6, 6]
+        index_valid(x) = i->(isvalid(x, i) ? 1 : 0)
+        index_isend(x) = i->(LazyStrings.isend(x, x.i + i - 1) ? 1 : 0)
+                                          #     F  u  _  m  _
+                                          #  0  1  2  3  4  5  6
+        @test map(index_valid(x), 0:6)   == [0, 1, 1, 1, 0, 0, 0]
+        @test map(index_isend(x), 0:6)   == [0, 0, 0, 0, 0, 1, 1]
+        @test map(i->thisind(x, i), 0:4) == [0, 1, 2, 3, 3]
+        @test map(i->prevind(x, i), 1:6) ==    [0, 1, 2, 3, 3, 3]
+        @test map(i->nextind(x, i), 0:2) == [1, 2, 3]
 
         @test map(i->iterate(x, i), 1:6) == [('F', 2),
                                              ('u', 3),
@@ -121,7 +136,39 @@ for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
                                              nothing]
 
         @test_throws BoundsError prevind(x, 0)
-        @test_throws BoundsError nextind(x, 6)
+        @test_throws BoundsError nextind(x, ncodeunits(x) + 1)
+    end
+end
+
+for pada in [0, 1, 7, 1234], padb in [0, 1, 7, 1234]
+    s = " u_m_"
+    pads = repeat(" ", pada) * s * "\n" * repeat(" ", padb)
+
+    for x in [TestLazyASCIIB(pads, pada + 1)]
+
+        @test x == "um"
+        @test x == String(x)
+        @test x == SubString(x)
+
+        index_valid(x) = i->(isvalid(x, i) ? 1 : 0)
+        index_isend(x) = i->(LazyStrings.isend(x, x.i + i - 1) ? 1 : 0)
+                                          #    ' ' u  _  m  _
+                                          #  0  1  2  3  4  5  6
+        @test map(index_valid(x), 0:6)   == [0, 1, 0, 1, 0, 0, 0]
+        @test map(index_isend(x), 0:6)   == [0, 0, 0, 0, 0, 1, 1]
+        @test map(i->thisind(x, i), 0:4) == [0, 1, 1, 3, 3]
+        @test map(i->prevind(x, i), 1:6) ==    [0, 1, 1, 3, 3, 3]
+        @test map(i->nextind(x, i), 0:2) == [1, 3, 3]
+
+        @test map(i->iterate(x, i), 1:6) == [('u', 3),
+                                             ('u', 3),
+                                             ('m', 5),
+                                             ('m', 5),
+                                             nothing,
+                                             nothing]
+
+        @test_throws BoundsError prevind(x, 0)
+        @test_throws BoundsError nextind(x, ncodeunits(x) + 1)
     end
 end
 
