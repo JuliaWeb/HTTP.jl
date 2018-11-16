831fda4389740f4b24202112657f058236b15cb2
Fredrik Ekre <ekrefredrik@gmail.com>
2018-11-16 19:27:15 +0100

    Upgrade to Documenter v0.20 (and actually deploy docs).

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,10 @@ using Documenter, HTTP
 
 makedocs(
     modules = [HTTP],
-    format = :html,
     sitename = "HTTP.jl",
     pages = ["Home" => "index.md"]
 )
 
 deploydocs(
     repo = "github.com/JuliaWeb/HTTP.jl.git",
-    target = "build",
-    deps = nothing,
-    make = nothing,
-    julia = "0.6",
-    osname = "linux"
 )
