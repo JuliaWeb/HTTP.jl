a2f78d342f7242578d699c3d60cdbddfab16a2e3
Eric Forgy <Eric.Forgy@coherentasia.com>
2019-03-09 17:10:01 +0800

    Expand Server type and use in `listen` and `listenloop`
    Export Server
    
    Fix for Julia v1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,22 +5,24 @@ using Test
 using HTTP
 
 @testset "HTTP" begin
-    for f in ["ascii.jl",
-              "issue_288.jl",
-              "utils.jl",
-              "sniff.jl",
-              "uri.jl",
-              "url.jl",
-              "cookies.jl",
-              "parser.jl",
-              "loopback.jl",
-              "WebSockets.jl",
-              "messages.jl",
-              "handlers.jl",
-              "server.jl",
-              "async.jl",
-              "aws4.jl"]
-
+    for f in [
+        "ascii.jl",
+        "issue_288.jl",
+        "utils.jl",
+        "sniff.jl",
+        "uri.jl",
+        "url.jl",
+        "cookies.jl",
+        "parser.jl",
+        "loopback.jl",
+        "WebSockets.jl",
+        "messages.jl",
+        "handlers.jl",
+        "server.jl",
+        "async.jl",
+        "aws4.jl"
+        ]
+        
         println("Running $f tests...")
         include(f)
     end
