bba5bb6f841cf3f2ccd9ed4bfc0cb63f4092f6ba
Fredrik Ekre <ekrefredrik@gmail.com>
2019-11-15 20:27:59 +0100

    Remove superfluous HTTP.post(uri, h, f::Form) method and schedule non-conforming method HTTP.post(uri, f::Form) method for deprecation in the next breaking release.

--- test/multipart.jl
+++ test/multipart.jl
@@ -9,4 +9,16 @@
             @test startswith(json["headers"]["Content-Type"], "multipart/form-data; boundary=")
         end
     end
+    @testset "Deprecation of HTTP.post without header for body::Form" begin
+        proj = normpath(joinpath(pathof(HTTP), "..", "..", "Project.toml"))
+        # Extract version = "(...)" from Project.toml
+        vers = VersionNumber(match(r"^version\s*=\s*\"(.*?)\"$"m, read(proj, String)).captures[1])
+        if vers.minor == 8 # this version
+            HTTP.post(uri, body).status == 200
+        elseif vers.minor == 9 || vers.major == 1 # Next breaking release
+            @test_logs (:warn, r"deprecated") HTTP.post(uri, body).status == 200
+        else # two breaking versions from now
+            @test_throws MethodError HTTP.post(uri, body)
+        end
+    end
 end
