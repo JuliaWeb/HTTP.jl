b2061647977adfa241275f8fe24d8e7d4bc2c817
Jacob Quinn <quinn.jacobd@gmail.com>
2019-12-07 08:23:46 -0700

    Make basic authorization automatically detected. Fixes #460 (#481)
    * Make basic authorization automatically detected. Fixes #460
    
    * fix tests

--- test/insert_layers.jl
+++ test/insert_layers.jl
@@ -5,21 +5,21 @@ using ..TestRequest
 
 @testset "HTTP Stack Inserting" begin
     @testset "Insert - Beginning" begin
-        expected = TestLayer{RedirectLayer{MessageLayer{RetryLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{Union{}}}}}}}}
+        expected = TestLayer{RedirectLayer{BasicAuthLayer{MessageLayer{RetryLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{Union{}}}}}}}}}
         result = insert(stack(), RedirectLayer, TestLayer)
 
         @test expected == result
     end
 
     @testset "Insert - Middle" begin
-        expected = RedirectLayer{MessageLayer{RetryLayer{TestLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{Union{}}}}}}}}
+        expected = RedirectLayer{BasicAuthLayer{MessageLayer{RetryLayer{TestLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{Union{}}}}}}}}}
         result = insert(stack(), ExceptionLayer, TestLayer)
 
         @test expected == result
     end
 
     @testset "Insert - End" begin
-        expected = RedirectLayer{MessageLayer{RetryLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{TestLayer{Union{}}}}}}}}
+        expected = RedirectLayer{BasicAuthLayer{MessageLayer{RetryLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{TestLayer{Union{}}}}}}}}}
         result = insert(stack(), Union{}, TestLayer)
 
         @test expected == result
@@ -32,7 +32,7 @@ using ..TestRequest
     @testset "Insert - Multiple Same layer" begin
         test_stack = insert(stack(), RetryLayer, ExceptionLayer)
 
-        expected = RedirectLayer{MessageLayer{TestLayer{ExceptionLayer{RetryLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{Union{}}}}}}}}}
+        expected = RedirectLayer{BasicAuthLayer{MessageLayer{TestLayer{ExceptionLayer{RetryLayer{ExceptionLayer{ConnectionPoolLayer{StreamLayer{Union{}}}}}}}}}}
         result = insert(test_stack, ExceptionLayer, TestLayer)
 
         @test expected == result
