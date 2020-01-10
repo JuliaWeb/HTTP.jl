5c5803115b13c0b7d4c077671bbe3730df750745
Joshua Gentry <pixel@deathbysoftware.com>
2020-01-09 20:00:34 -0500

    Fixed the test for the show method on the Multipart object.

--- test/multipart.jl
+++ test/multipart.jl
@@ -2,17 +2,11 @@
 @testset "Multipart" begin
 	@testset "show" begin
 		# testing that there is no error in printing when nothing is set for filename
-		try
-			show(HTTP.Multipart(nothing, IOBuffer("some data"), "plain/text", "", "testname"))
-			println("")
-			@test true
-		catch exception
-			@error "" typeof(exception) exception
-			@test false
-		end
+		str = sprint(show, (HTTP.Multipart(nothing, IOBuffer("some data"), "plain/text", "", "testname")))
+		@test findfirst("contenttype=\"plain/text\"", str) != nothing
 	end
-	
-	
+
+
 	@testset "constructor" begin
 		@testset "don't allow String for data" begin
 			@test_throws MethodError HTTP.Multipart(nothing, "some data", "plain/text", "", "testname")
