65da6e9ea2b6306e746b067e1950c3857e6ec588
Chris de Graaf <me@cdg.dev>
2020-05-23 21:16:47 -0500

    Add a test

--- test/resources/TestRequest.jl
+++ test/resources/TestRequest.jl
@@ -1,10 +1,18 @@
 module TestRequest
-	import HTTP: Layer, request, Response
+import HTTP: Layer, request, Response
 
-	abstract type TestLayer{Next <: Layer} <: Layer{Next} end
-	export TestLayer, request
+abstract type TestLayer{Next <: Layer} <: Layer{Next} end
+abstract type LastLayer{Next <: Layer} <: Layer{Next} end
+export TestLayer, LastLayer, request
 
-	function request(::Type{TestLayer{Next}}, io::IO, req, body; kw...)::Response where Next
+function request(::Type{TestLayer{Next}}, io::IO, req, body; kw...)::Response where Next
 		return request(Next, io, req, body; kw...)
-	end
+end
+
+const FLAG = Ref(false)
+function request(::Type{LastLayer{Next}}, resp)::Response where Next
+    FLAG[] = true
+		return request(Next, resp)
+end
+
 end
