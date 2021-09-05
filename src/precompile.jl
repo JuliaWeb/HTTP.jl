b6d001007df88d7e54f1cdeeefca9231183244f9
Rik Huijzer <rikhuijzer@pm.me>
2021-09-05 10:10:23 +0200

    Precompile show too

--- src/precompile.jl
+++ src/precompile.jl
@@ -6,7 +6,9 @@ Run some tiny workloads that the package should precompile.
 function _precompile_()
     VERSION < v"1.5" && return nothing
     try
-        HTTP.get("http://127.0.0.1"; connect_timeout=1, retry=false)
+        r = HTTP.get("http://127.0.0.1"; connect_timeout=1, retry=false)
+        io = IOBuffer()
+        show(io, r)
     catch
     end
 end
