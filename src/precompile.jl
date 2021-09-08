ebbdb2cb927d66a1b1b932d4194786a04bfd94aa
Rik Huijzer <rikhuijzer@pm.me>
2021-09-08 13:57:41 +0200

    Add ccall

--- src/precompile.jl
+++ src/precompile.jl
@@ -4,7 +4,9 @@
 Run some tiny workloads that the package should precompile.
 """
 function _precompile_()
+    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     VERSION < v"1.5" && return nothing
+
     try
         r = HTTP.get("http://127.0.0.1"; connect_timeout=1, retry=false)
         io = IOBuffer()
