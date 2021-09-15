f407e7202cd03f921688baa72cd7a755aa07f2a2
Fredrik Ekre <ekrefredrik@gmail.com>
2021-09-15 10:32:43 +0200

    Make sure that the API secret is not empty.

--- test/client.jl
+++ test/client.jl
@@ -178,7 +178,7 @@ end
     end
 end
 
-if haskey(ENV, "PIE_SOCKET_API_KEY")
+if !isempty(get(ENV, "PIE_SOCKET_API_KEY", ""))
     println("found pie socket api key, running websocket tests")
     pie_socket_api_key = ENV["PIE_SOCKET_API_KEY"]
     @testset "openraw client method - $socket_protocol" for socket_protocol in ["wss", "ws"]
