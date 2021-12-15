5fccc91fdac9c11e8b86d158d2c60268dfbee2ff
Fredrik Ekre <ekrefredrik@gmail.com>
2021-12-15 11:41:58 +0100

    Try to debug windows.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,38 @@ using Test, HTTP, JSON
 const dir = joinpath(dirname(pathof(HTTP)), "..", "test")
 include(joinpath(dir, "resources/TestRequest.jl"))
 
+
+using Sockets
+@testset "Incomplete response with known content length" begin
+    server = Sockets.listen(ip"0.0.0.0", 8080)
+    try
+        task = @async HTTP.listen("0.0.0.0", 8080; server=server) do http
+            HTTP.setstatus(http, 200)
+            HTTP.setheader(http, "Content-Length" => "64") # Promise 64 bytes...
+            HTTP.startwrite(http)
+            HTTP.write(http, rand(UInt8, 63)) # ...but only send 63 bytes.
+            # Close the stream so that eof(stream) is true and the client isn't
+            # waiting forever for the last byte.
+            HTTP.close(http.stream)
+        end
+
+        err = try
+            HTTP.get("http://localhost:8080"; retry=false)
+        catch err
+            err
+        end
+        @test err isa HTTP.IOError
+        @test err.e isa EOFError
+
+    finally
+        # Shutdown
+        try; close(server); wait(task); catch; end
+        HTTP.ConnectionPool.closeall()
+    end
+end
+
+exit(0)
+
 @testset "HTTP" begin
     for f in [
               "ascii.jl",
