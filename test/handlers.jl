5ed226c709454e8eb745a8912ea7d3e875a4be2b
Jacob Quinn <quinn.jacobd@gmail.com>
2022-03-15 21:04:45 -0600

    Remove use of at-eval in handlers register macro

--- test/handlers.jl
+++ test/handlers.jl
@@ -1,8 +1,8 @@
+module TestHandlers
+
 using HTTP
 using Test
 import Base.==
-
-include("resources/FooRouter.jl")
 
 ==(a::HTTP.Response,b::HTTP.Response) = (a.status  == b.status)    &&
                                         (a.version == b.version)   &&
@@ -11,9 +11,12 @@ include("resources/FooRouter.jl")
 
 @testset "Handlers" begin
     @testset "FooRouter" begin
+        r = HTTP.Router()
+        f = HTTP.Handlers.RequestHandlerFunction((req) -> HTTP.Response(200))
+        HTTP.@register(r, "/test", f)
         req = HTTP.Request()
         req.target = "/test"
-        @test HTTP.handle(FooRouter.r, req) == HTTP.Response(200)
+        @test HTTP.handle(r, req) == HTTP.Response(200)
     end
 
     @testset "HTTP Router" begin
@@ -29,9 +32,8 @@ include("resources/FooRouter.jl")
         req.target = "/path/to/greatness"
         @test HTTP.handle(r, req) == HTTP.Response(200)
 
-        p = "/next/path/to/greatness"
         f2 = HTTP.Handlers.RequestHandlerFunction((req) -> HTTP.Response(201))
-        HTTP.@register(r, p, f2)
+        HTTP.@register(r, "/next/path/to/greatness", f2)
         req = HTTP.Request()
         req.target = "/next/path/to/greatness"
         @test HTTP.handle(r, req) == HTTP.Response(201)
@@ -75,62 +77,6 @@ include("resources/FooRouter.jl")
         @test HTTP.handle(r, req) == HTTP.Response(203)
     end
 
-    @testset "Deprecation tests" begin
-        f = HTTP.Handlers.RequestHandlerFunction((req) -> HTTP.Response(200))
-        @test HTTP.handle(f, HTTP.Request()) == HTTP.Response(200)
-
-        r = HTTP.Router()
-        @test isempty(r.routes)
-        @test HTTP.handle(r, HTTP.Request()) == HTTP.Response(404)
-
-        HTTP.@register(r, "/path/to/greatness", f)
-        req = HTTP.Request()
-        req.target = "/path/to/greatness"
-        @test HTTP.handle(r, req) == HTTP.Response(200)
-
-        p = "/next/path/to/greatness"
-        f2 = HTTP.Handlers.RequestHandlerFunction((req) -> HTTP.Response(201))
-        HTTP.@register(r, p, f2)
-        req = HTTP.Request()
-        req.target = "/next/path/to/greatness"
-        @test HTTP.handle(r, req) == HTTP.Response(201)
-
-        r = HTTP.Router()
-        HTTP.@register(r, "GET", "/sget", f)
-        HTTP.@register(r, "POST", "/spost", f)
-        HTTP.@register(r, "POST", "/tpost", f)
-        req = HTTP.Request("GET", "/sget")
-        @test HTTP.handle(r, req) == HTTP.Response(200)
-        req = HTTP.Request("POST", "/sget")
-        @test HTTP.handle(r, req) == HTTP.Response(404)
-        req = HTTP.Request("GET", "/spost")
-        @test HTTP.handle(r, req) == HTTP.Response(404)
-        req = HTTP.Request("POST", "/spost")
-        @test HTTP.handle(r, req) == HTTP.Response(200)
-        req = HTTP.Request("GET", "/tpost")
-        @test HTTP.handle(r, req) == HTTP.Response(404)
-        req = HTTP.Request("POST", "/tpost")
-        @test HTTP.handle(r, req) == HTTP.Response(200)
-
-        r = HTTP.Router()
-        HTTP.@register(r, "/test", f)
-        HTTP.@register(r, "/test/*", f2)
-        f3 = HTTP.Handlers.RequestHandlerFunction((req) -> HTTP.Response(202))
-        HTTP.@register(r, "/test/sarv/ghotra", f3)
-        f4 = HTTP.Handlers.RequestHandlerFunction((req) -> HTTP.Response(203))
-        HTTP.@register(r, "/test/*/ghotra/seven", f4)
-
-        req = HTTP.Request()
-        req.target = "/test"
-        @test HTTP.handle(r, req) == HTTP.Response(200)
-
-        req.target = "/test/sarv"
-        @test HTTP.handle(r, req) == HTTP.Response(201)
-
-        req.target = "/test/sarv/ghotra"
-        @test HTTP.handle(r, req) == HTTP.Response(202)
-
-        req.target = "/test/sar/ghotra/seven"
-        @test HTTP.handle(r, req) == HTTP.Response(203)
-    end
 end
+
+end # module
