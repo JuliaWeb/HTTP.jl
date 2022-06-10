d9a61493a5cbaa1a3b0dd98c35d7b3e4e5453877
Jacob Quinn <quinn.jacobd@gmail.com>
2022-06-10 14:15:06 -0600

    try running autobahn as separate job in autobahn docker container

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,37 @@
 using Test, HTTP, JSON
 
 const dir = joinpath(dirname(pathof(HTTP)), "..", "test")
-include(joinpath(dir, "resources/TestRequest.jl"))
 
-@testset "HTTP" begin
-    for f in [
-              "ascii.jl",
-              "chunking.jl",
-              "utils.jl",
-              "client.jl",
-              "multipart.jl",
-              "parsemultipart.jl",
-              "sniff.jl",
-              "cookies.jl",
-              "parser.jl",
-              "loopback.jl",
-              "websockets/deno_client/server.jl",
-              "websockets/websockets.jl",
-              "messages.jl",
-              "handlers.jl",
-              "server.jl",
-              "async.jl",
-              "mwe.jl",
-             ]
-        file = joinpath(dir, f)
-        println("Running $file tests...")
-        if isfile(file)
-            include(file)
-        else
-            @show readdir(dirname(file))
+if !isempty(get(ENV, "AUTOBAHN_WEBSOCKETS_TEST", ""))
+    include("websockets/autobahn.jl")
+else
+    include(joinpath(dir, "resources/TestRequest.jl"))
+    @testset "HTTP" begin
+        for f in [
+                "ascii.jl",
+                "chunking.jl",
+                "utils.jl",
+                "client.jl",
+                "multipart.jl",
+                "parsemultipart.jl",
+                "sniff.jl",
+                "cookies.jl",
+                "parser.jl",
+                "loopback.jl",
+                "websockets/deno_client/server.jl",
+                "messages.jl",
+                "handlers.jl",
+                "server.jl",
+                "async.jl",
+                "mwe.jl",
+                ]
+            file = joinpath(dir, f)
+            println("Running $file tests...")
+            if isfile(file)
+                include(file)
+            else
+                @show readdir(dirname(file))
+            end
         end
     end
 end
