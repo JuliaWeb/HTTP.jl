7e9b0fdc0cc67b9557bac5e217de8c08a1c6a15e
Nick Robinson <npr251@gmail.com>
2022-11-01 11:18:53 +0000

    Update src/clientlayers/MessageRequest.jl

--- src/clientlayers/MessageRequest.jl
+++ src/clientlayers/MessageRequest.jl
@@ -14,7 +14,7 @@ Construct a [`Request`](@ref) object from method, url, headers, and body.
 Hard-coded as the first layer in the request pipeline.
 """
 function messagelayer(handler)
-    return function(method::String, url::URI, headers::Headers, body; response_stream=nothing, http_version=HTTPVersion(1,1), kw...)
+    return function(method::String, url::URI, headers::Headers, body; response_stream=nothing, http_version=HTTPVersion(1, 1), kw...)
         req = Request(method, resource(url), headers, body; url=url, version=http_version, responsebody=response_stream)
         local resp
         try
