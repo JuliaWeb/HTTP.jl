4eb724c92c754cad1705dc77190a76a56c87f24c
Jacob Quinn <quinn.jacobd@gmail.com>
2022-11-01 23:04:56 -0600

    try again

--- test/websockets/autobahn.jl
+++ test/websockets/autobahn.jl
@@ -77,12 +77,15 @@ end # @testset "Autobahn testsuite"
             send(ws, msg)
         end
     end
-    rm(joinpath(DIR, "reports/server/index.json"); force=true)
-    @test success(run(Cmd(`docker run --rm --network="host" --add-host host.docker.internal:host-gateway -v "$DIR/config:/config" -v "$DIR/reports:/reports" --name fuzzingclient crossbario/autobahn-testsuite wstest -m fuzzingclient -s /config/fuzzingclient.json`; dir=DIR), stdin, stdout, stdout; wait=false))
-    close(server)
-    report = JSON.parsefile(joinpath(DIR, "reports/server/index.json"))
-    for (k, v) in pairs(report["main"])
-        @test v["behavior"] in ("OK", "NON-STRICT", "INFORMATIONAL", "UNIMPLEMENTED")
+    try
+        rm(joinpath(DIR, "reports/server/index.json"); force=true)
+        @test success(run(Cmd(`docker run --rm --net="host" -v "$DIR/config:/config" -v "$DIR/reports:/reports" --name fuzzingclient crossbario/autobahn-testsuite wstest -m fuzzingclient -s /config/fuzzingclient.json`; dir=DIR), stdin, stdout, stdout; wait=false))
+        report = JSON.parsefile(joinpath(DIR, "reports/server/index.json"))
+        for (k, v) in pairs(report["main"])
+            @test v["behavior"] in ("OK", "NON-STRICT", "INFORMATIONAL", "UNIMPLEMENTED")
+        end
+    finally
+        close(server)
     end
 end
 
