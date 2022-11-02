2a250a223283a44d8585f40bad47d75d27e8c347
Jacob Quinn <quinn.jacobd@gmail.com>
2022-11-01 20:59:03 -0600

    Switch to using docker for autobahn tests

--- test/websockets/autobahn.jl
+++ test/websockets/autobahn.jl
@@ -2,62 +2,73 @@ using Test, Sockets, HTTP, HTTP.WebSockets, JSON
 
 const DIR = abspath(joinpath(dirname(pathof(HTTP)), "../test/websockets"))
 
+havedocker = success(`which docker`)
+!havedocker && @warn "Docker not found, skipping Autobahn tests"
+
 # 32-bit not supported by autobahn
-if Int === Int64 && !Sys.iswindows()
+if Int === Int64 && !Sys.iswindows() && havedocker
+
+if length(split(read(`docker images crossbario/autobahn-testsuite`, String), '\n'; keepempty=false)) < 2
+    @assert success(`docker pull crossbario/autobahn-testsuite`)
+end
 
 @testset "Autobahn WebSocket Tests" begin
 
 @testset "Client" begin
-    serverproc = run(Cmd(`wstest -u 0 -m fuzzingserver -s config/fuzzingserver.json`; dir=DIR), stdin, stdout, stdout; wait=false)
-    sleep(5) # give time for server to get setup
-    cases = Ref(0)
-    runtests = Ref(true)
+    # Run the autobahn test suite in a docker container
+    serverproc = run(Cmd(`docker run -it --rm -v "$DIR/config:/config" -v "$DIR/reports:/reports" -p 9001:9001 --name fuzzingserver crossbario/autobahn-testsuite`; dir=DIR), stdin, stdout, stdout; wait=false)
     try
-        WebSockets.open("ws://127.0.0.1:9001/getCaseCount") do ws
-            for msg in ws
-                cases[] = parse(Int, msg)
+        sleep(5) # give time for server to get setup
+        cases = Ref(0)
+        runtests = Ref(true)
+        try
+            WebSockets.open("ws://127.0.0.1:9001/getCaseCount") do ws
+                for msg in ws
+                    cases[] = parse(Int, msg)
+                end
             end
+        catch e
+            @error "problem getting autobahn case count" exception=(e, catch_backtrace())
+            @show serverproc
+            runtests[] = false
         end
-    catch e
-        @error "problem getting autobahn case count" exception=(e, catch_backtrace())
-        @show serverproc
-        runtests[] = false
-    end
 
-    if runtests[]
-        for i = 1:cases[]
-            println("Running test case = $i")
-            verbose = false
-            try
-                WebSockets.open("ws://127.0.0.1:9001/runCase?case=$(i)&agent=main"; verbose, suppress_close_error=true) do ws
-                    for msg in ws
-                        send(ws, msg)
+        if runtests[]
+            for i = 1:cases[]
+                println("Running test case = $i")
+                verbose = false
+                try
+                    WebSockets.open("ws://127.0.0.1:9001/runCase?case=$(i)&agent=main"; verbose, suppress_close_error=true) do ws
+                        for msg in ws
+                            send(ws, msg)
+                        end
                     end
+                catch
+                    # ignore errors here since we want to run all cases + some are expected to throw
+                end
+            end
+
+            rm(joinpath(DIR, "reports/clients/index.json"); force=true)
+            sleep(1)
+            try
+                WebSockets.open("ws://127.0.0.1:9001/updateReports?agent=main") do ws
+                    receive(ws)
                 end
             catch
-                # ignore errors here since we want to run all cases + some are expected to throw
+                WebSockets.open("ws://127.0.0.1:9001/updateReports?agent=main") do ws
+                    receive(ws)
+                end
             end
-        end
 
-        rm(joinpath(DIR, "reports/clients/index.json"); force=true)
-        sleep(1)
-        try
-            WebSockets.open("ws://127.0.0.1:9001/updateReports?agent=main") do ws
-                receive(ws)
-            end
-        catch
-            WebSockets.open("ws://127.0.0.1:9001/updateReports?agent=main") do ws
-                receive(ws)
+            report = JSON.parsefile(joinpath(DIR, "reports/clients/index.json"))
+            for (k, v) in pairs(report["main"])
+                @test v["behavior"] in ("OK", "NON-STRICT", "INFORMATIONAL")
             end
         end
-        
-        report = JSON.parsefile(joinpath(DIR, "reports/clients/index.json"))
-        for (k, v) in pairs(report["main"])
-            @test v["behavior"] in ("OK", "NON-STRICT", "INFORMATIONAL")
-        end
+    finally
+        # stop/remove server process
+        kill(serverproc)
     end
-    # stop/remove server process
-    kill(serverproc)
 end # @testset "Autobahn testsuite"
 
 @testset "Server" begin
@@ -67,7 +78,7 @@ end # @testset "Autobahn testsuite"
         end
     end
     rm(joinpath(DIR, "reports/server/index.json"); force=true)
-    @test success(run(Cmd(`wstest -u 0 -m fuzzingclient -s config/fuzzingclient.json`; dir=DIR)))
+    @test success(run(Cmd(`docker run -it --rm --add-host host.docker.internal:host-gateway -v "$DIR/config:/config" -v "$DIR/reports:/reports" --name fuzzingclient crossbario/autobahn-testsuite wstest -m fuzzingclient -s /config/fuzzingclient.json`; dir=DIR), stdin, stdout, stdout; wait=false))
     close(server)
     report = JSON.parsefile(joinpath(DIR, "reports/server/index.json"))
     for (k, v) in pairs(report["main"])
