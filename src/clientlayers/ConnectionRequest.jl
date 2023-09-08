429017272105a2dd8db6897bfb354fd5d89a9851
Jacob Quinn <quinn.jacobd@gmail.com>
2023-09-08 16:06:56 -0600

    Fix a few typos

--- src/clientlayers/ConnectionRequest.jl
+++ src/clientlayers/ConnectionRequest.jl
@@ -119,13 +119,14 @@ function connectionlayer(handler)
             stream = Stream(req.response, io)
             return handler(stream; readtimeout=readtimeout, logerrors=logerrors, logtag=logtag, kw...)
         catch e
+            shouldreuse = false
             # manually unwrap CompositeException since it's not defined as a "wrapper" exception by ExceptionUnwrapping
             while e isa CompositeException
                 e = e.exceptions[1]
             end
             root_err = ExceptionUnwrapping.unwrap_exception_to_root(e)
             # don't log if it's an HTTPError since we should have already logged it
-            if logerrors && err isa StatusError
+            if logerrors && root_err isa StatusError
                 err = current_exceptions_to_string()
                 @error err type=Symbol("HTTP.StatusError") method=req.method url=req.url context=req.context logtag=logtag
             end
@@ -133,8 +134,7 @@ function connectionlayer(handler)
                 err = current_exceptions_to_string(e)
                 @error err type=Symbol("HTTP.ConnectionRequest") method=req.method url=req.url context=req.context logtag=logtag
             end
-            @debugv 1 "❗️  ConnectionLayer $e. Closing: $io"
-            shouldreuse = false
+            @debugv 1 "❗️  ConnectionLayer $root_err. Closing: $io"
             if @isdefined(stream) && stream.nwritten == -1
                 # we didn't write anything, so don't need to worry about
                 # idempotency of the request
