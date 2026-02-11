5856367efcd8453b259f2df75f5e8122d30b941a
Jacob Quinn <jacob.quinn@cbtravel.com>
2026-02-11 00:27:02 -0700

    fix(client): restore retry loop and token scheduling

--- src/client/retry.jl
+++ src/client/retry.jl
@@ -32,6 +32,8 @@ function isrecoverable(ex::Exception)::Bool
             isrecoverable(child) || return false
         end
         return true
+    elseif ex isa Reseau.Sockets.DNSError
+        return Int(ex.code) == Base.UV_EAI_AGAIN
     elseif ex isa AWSError
         return true
     end
@@ -77,6 +79,55 @@ function _retry_error_type(err)
     return Reseau.Sockets.RetryErrorType.TRANSIENT
 end
 
+function _set_nretries!(x, nretries::Int)
+    if x isa Response
+        x.metrics.nretries = nretries
+    elseif x isa StatusError
+        x.response.metrics.nretries = nretries
+    elseif x isa RequestError
+        _set_nretries!(x.error, nretries)
+    elseif x isa StreamError && x.stream !== nothing
+        x.stream.response !== nothing && (x.stream.response.metrics.nretries = nretries)
+    end
+    return nothing
+end
+
+function _acquire_retry_token!(client::Client, partition)
+    fut = Future{Reseau.Sockets.StandardRetryToken}()
+    on_acquired = function(token, error_code, fut_ref)
+        fut_local = fut_ref::Future{Reseau.Sockets.StandardRetryToken}
+        if error_code != Reseau.OP_SUCCESS || token === nothing
+            notify(fut_local, aws_error(error_code != Reseau.OP_SUCCESS ? error_code : Reseau.ERROR_INVALID_STATE))
+        else
+            notify(fut_local, token)
+        end
+        return nothing
+    end
+    Reseau.Sockets.retry_strategy_acquire_token!(
+        client.retry_strategy,
+        partition,
+        on_acquired,
+        fut,
+        client.settings.retry_timeout_ms,
+    )
+    return wait(fut)
+end
+
+function _schedule_retry!(token, error_type::Reseau.Sockets.RetryErrorType.T)
+    fut = Future{typeof(token)}()
+    on_ready = function(token_cb, error_code, fut_ref)
+        fut_local = fut_ref::Future{typeof(token)}
+        if error_code != Reseau.OP_SUCCESS || token_cb === nothing
+            notify(fut_local, aws_error(error_code != Reseau.OP_SUCCESS ? error_code : Reseau.ERROR_INVALID_STATE))
+        else
+            notify(fut_local, token_cb)
+        end
+        return nothing
+    end
+    Reseau.Sockets.retry_token_schedule_retry(token, error_type, on_ready, fut)
+    return wait(fut)
+end
+
 function with_retry_token(
     f,
     client::Client;
@@ -91,26 +142,134 @@ function with_retry_token(
     req_ref=nothing,
     context=nothing,
 )
-    start_time = context !== nothing ? time() : 0.0
-    try
-        return f()
-    catch e
-        err = e isa StreamError ? (e::StreamError).error : e
-        if err isa DontRetry
-            if e isa StreamError
-                stream = (e::StreamError).stream::Stream
-                if iserror(stream.response.status) && stream.bufferstream !== nothing
-                    # For error responses, we need to commit the temporary body buffer.
+    retry_token = nothing
+    partition = client.settings.retry_partition
+    use_retry_strategy = retry_delays === nothing && partition !== nothing
+
+    max_retries = client.settings.max_retries
+    if max_retries == 0
+        start_time = context === nothing ? 0.0 : time()
+        try
+            return f()
+        catch e
+            stream = nothing
+            err = e
+            if err isa StreamError
+                stream = err.stream
+                err = err.error
+            end
+            if err isa DontRetry
+                if stream !== nothing && iserror(stream.response.status) && stream.bufferstream !== nothing
                     stream.response.body = readavailable(stream.bufferstream)
                 end
+                err = err.error
             end
-            throw(err.error)
+            if logerrors
+                @error "HTTP request error" exception=(err, catch_backtrace()) method=method url=uri logtag=logtag
+            end
+            throw(err)
+        finally
+            context !== nothing && _record_layer!(context, :retrylayer, start_time)
         end
-        if logerrors
-            @error "HTTP request error" exception=(err, catch_backtrace()) method=method url=uri logtag=logtag
+    end
+
+    retry_check_fn = retry_check === nothing ? nothing : retry_check
+    delays = _normalize_retry_delays(retry_delays, max_retries)
+    delay_state = nothing
+    nretries = 0
+
+    while true
+        attempt_start = context === nothing ? 0.0 : time()
+        try
+            ret = f()
+            context !== nothing && _record_layer!(context, :retrylayer, attempt_start)
+            _set_nretries!(ret, nretries)
+            if retry_token !== nothing
+                Reseau.Sockets.retry_token_record_success(retry_token)
+                Reseau.Sockets.retry_token_release!(retry_token)
+                retry_token = nothing
+            end
+            return ret
+        catch e
+            context !== nothing && _record_layer!(context, :retrylayer, attempt_start)
+
+            stream = nothing
+            err = e
+            if err isa StreamError
+                stream = err.stream
+                err = err.error
+            end
+
+            if logerrors
+                log_err = err isa DontRetry ? err.error : err
+                @error "HTTP request error" exception=(log_err, catch_backtrace()) method=method url=uri logtag=logtag
+            end
+
+            if err isa DontRetry
+                if stream !== nothing && iserror(stream.response.status) && stream.bufferstream !== nothing
+                    stream.response.body = readavailable(stream.bufferstream)
+                end
+                err = err.error
+                _set_nretries!(err, nretries)
+                if retry_token !== nothing
+                    Reseau.Sockets.retry_token_release!(retry_token)
+                    retry_token = nothing
+                end
+                throw(err)
+            end
+
+            if nretries >= max_retries
+                _set_nretries!(err, nretries)
+                if retry_token !== nothing
+                    Reseau.Sockets.retry_token_release!(retry_token)
+                    retry_token = nothing
+                end
+                throw(err)
+            end
+
+            delay = 0.0
+            if !use_retry_strategy
+                delay_iter = delay_state === nothing ? iterate(delays) : iterate(delays, delay_state)
+                delay_iter === nothing && (_set_nretries!(err, nretries); throw(err))
+                delay, delay_state = delay_iter
+            end
+
+            req = req_ref === nothing ? nothing : req_ref[]
+            resp = err isa StatusError ? err.response : nothing
+            resp_body = resp === nothing ? nothing : resp.body
+            retry = _default_retryable(method, err, retryable_body, retry_non_idempotent)
+            if !retry && retry_check_fn !== nothing && retryable_body
+                retry = retry_check_fn(delay, err, req, resp, resp_body)
+            end
+            if !retry
+                _set_nretries!(err, nretries)
+                if retry_token !== nothing
+                    Reseau.Sockets.retry_token_release!(retry_token)
+                    retry_token = nothing
+                end
+                throw(err)
+            end
+
+            if use_retry_strategy
+                try
+                    if retry_token === nothing
+                        retry_token = _acquire_retry_token!(client, partition)
+                    end
+                    retry_token = _schedule_retry!(retry_token, _retry_error_type(err))
+                catch
+                    if retry_token !== nothing
+                        Reseau.Sockets.retry_token_release!(retry_token)
+                        retry_token = nothing
+                    end
+                    _set_nretries!(err, nretries)
+                    throw(err)
+                end
+                nretries += 1
+                continue
+            end
+
+            nretries += 1
+            _task_sleep_s(delay)
         end
-        rethrow()
-    finally
-        context !== nothing && _record_layer!(context, :retrylayer, start_time)
     end
 end
